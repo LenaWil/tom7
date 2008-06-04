1fe4e5a64214fa5b87b96f4f6f18ad2073cbef3e
tom7 <tom7@cs.cmu.edu>
2008-06-04 03:01:53 +0000

    improve lost mode

--- sitelisp/lost.el
+++ sitelisp/lost.el
@@ -1,5 +1,7 @@
 ;;; lost.el --- every 108 minutes the button must be pressed
 
+; (load "c:\\code\\sitelisp\\lost.el")
+; (autoload 'lost-mode "lost.el" "Lost mode" t)
 
 ;;; Author: Tom Murphy 7
 ;;; Based on type-break.el by Noah Friedman, GPL
@@ -25,19 +27,51 @@
 (defun lost-insist-swan-settings ()
   (interactive)
   (blink-cursor-mode 1)
-  (let* ((bg (assq 'background-color (frame-parameters)))
+  (local-set-key [13] 'lost-execute)
+  (let* ((cu (assq 'cursor-color (frame-parameters)))
+	 (bg (assq 'background-color (frame-parameters)))
 	 (fg (assq 'foreground-color (frame-parameters))))
     (if (not (and bg fg (string= (cdr bg) "black") (string= (cdr fg) "green")))
 	(progn
-	  (insert (cdr bg))
-	  (insert (cdr fg))
-	  (insert "YESSS")
+	  ; (insert (cdr bg))
+	  ; (insert (cdr fg))
+	  ; (insert "YESSS")
+	  (setq lost-old-background (cdr bg))
+	  (setq lost-old-foreground (cdr fg))
+	  (setq lost-old-cursor (cdr cu))
 	  (set-background-color "black")
 	  (set-foreground-color "green")
-	  (custom-set-faces '(cursor ((t (:background "green" :foreground "black")))))))))
+	  ;(custom-set-faces '(cursor ((t (:background "green" :foreground "black")))))
+	  (set-cursor-color "green")
 
+	  ))))
 
+;; assumes they were changed by lost-insist-swan-settings
+(defun lost-restore-settings ()
+  (set-foreground-color lost-old-foreground)
+  (set-background-color lost-old-background)
+  (set-cursor-color lost-old-cursor)
+  ;; XXX restore blink state of cursor
 ; (assq 'background-color (frame-parameters))
+)
+
+(defun lost-execute ()
+  (interactive "")
+  (let ((numbers-ok (save-excursion
+		      (goto-char (point-min))
+		      (looking-at ">: 4 8 15 16 23 42"))))
+    (cond (numbers-ok
+	   (lost-restore-settings)
+	   ;; should animate this?
+	   (setq lost-seconds (* 60 108))
+	   (kill-buffer "*swan*")
+	   (message "Every 108 minutes the button must be pressed.")
+	   ; (restore-old-settings)
+	   )
+	  (t (message "??")
+	     (lost-make-prompt)))
+
+    ))
 
 
 (defun lost-twodig (n)
@@ -112,32 +146,64 @@
 	     (looking-at ">: ")
 	     )))
       (cond ((not prompt-ok)
-	     (erase-buffer)
-	     (insert ">: ")
-	     (goto-char (point-max)))
-	    (prompt-ok 
-	     (message "ok")
-	     (sit-for 1)
-	     ))
-
+	     (lost-make-prompt)))
       ))) ; entry mode
 )
 
+(defun lost-make-prompt ()
+  (erase-buffer)
+  (insert ">: ")
+  (goto-char (point-max)))
+
 (setq lost-mode-string "LOST")
+;; Properties are ignored in emacs 22+ unless variable is risky.
+(put 'lost-mode-string 'risky-local-variable t)
 
 ;; from timeclock. find the global-mode-string symbol in the
 ;; modeline format. 
 (defun lost-set-modeline ()
-  (let ((list-entry (memq 'global-mode-string
-			  mode-line-format)))
-    (unless (or (null list-entry)
-		(memq 'lost-mode-string mode-line-format))
-      (setcdr list-entry
-	      (cons 'lost-mode-string
-		    (cdr list-entry))))
-    ))
+  ;; global-mode string has to start with a list or string
+  ;; if the contents are to be evaluated and treated as mode
+  ;; line formats.
+  (or global-mode-string (setq global-mode-string '("")))
+;  (let ((list-entry (memq 'global-mode-string
+;			  mode-line-format)))
+;    (unless (or (null list-entry)
+;		(memq 'lost-mode-string mode-line-format))
+;      (setcdr list-entry
+;	      (cons 'lost-mode-string
+;		    (cdr list-entry))))
+;    )
+  ;; have to put this at the end, or else it's interpreted
+  ;; as a condition.
+  (setq global-mode-string
+	(append global-mode-string '(lost-mode-string)))
+  (force-mode-line-update)
+  )
 
 (lost-set-modeline)
+
+; (setq mode-line-format (cons (propertize "awesome" 'face 'lost-hhour-face) mode-line-format))
+; (setq mode-line-format 'awesome)
+; (setq awesome (propertize "awesome" 'face 'lost-hhour-face))
+; (put 'awesome 'risky-local-variable t)
+
+
+; (setq lost-set-modeline (lambda ()))
+; (setq lost-periodic (lambda ()))
+
+; (assq 'global-mode-string mode-line-format)
+; (memq 'global-mode-string mode-line-modes)
+
+; (mapcar (lambda (x) (progn
+; 		      (insert " ")
+; 		      (cond ((stringp x) (insert x))
+; 			  ((symbolp x) (insert (symbol-name x)))
+; 			  (t (insert "?"))))) mode-line-format)
+; 
+; (setq global-mode-string "awesome")
+
+; symbol
 
 ; mode-line-format
 
@@ -149,8 +215,11 @@
 ; (setq lost-seconds (* 60 108))
 ; (setq lost-seconds (* 60 5))
 ; (setq lost-seconds 5)
+
+;;;;
 (run-at-time 0 1 'lost-periodic)
 
+;; emacs-version
 
 ;; 
 ;; 
