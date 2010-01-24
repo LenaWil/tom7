7b4379d6875ecdccc2d5204a8d07e0cdab15c4ed
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2010-01-24 17:20:54 +0000

    j/k now it compiles really

---
+++ trunk/escapex/backgrounds.h
@@ -0,0 +1,26 @@
+#ifndef __BACKGROUNDS_H
+#define __BACKGROUNDS_H
+
+#include "escapex.h"
+#include "draw.h"
+#include "sdlutil.h"
+#include "drawable.h"
+
+struct backgrounds {
+
+  /* Creates a checkerboard pattern of blocks with
+     a random vertical gradient atop it. Modifies
+     surf so that it points to an surface with the
+     same size as the screen, if it doesn't already. */
+  static void gradientblocks(SDL_Surface *& surf,
+			     int tile_white,
+			     int tile_black,
+			     float gradient_hue);
+
+  static const float blueish;
+
+ private:
+  backgrounds();
+};
+
+#endif
