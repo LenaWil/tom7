56fe6ec85b712d72668546914affb137f604734a
tom7 <tom7@cs.cmu.edu>
2010-12-18 20:56:01 +0000

    boss sits there and robo dances. you can jump on him or push him very slowlym, but only to the right ?

--- discovery/tiles.js
+++ discovery/tiles.js
@@ -6,5 +6,15 @@ var tiles =
  { id: 2, isbg: true,  frames: ['solidteal', 1] },
  { id: 3, isbg: true,  frames: ['solidtealv1', 1] },
  { id: 4, isbg: true,  frames: ['solidtealv2', 1] },
- { id: 5, isbg: true,  frames: ['solidtealv3', 1] }
+ { id: 5, isbg: true,  frames: ['solidtealv3', 1] },
+ { id: 6, isbg: true,  frames: ['vip1', 1] },
+ { id: 7, isbg: true,  frames: ['vip2', 1] },
+ { id: 8, isbg: true,  frames: ['cubetop1', 1] },
+ { id: 9, isbg: true,  frames: ['cubetop2', 1] },
+ { id: 10, isbg: true,  frames: ['cubetop3', 1] },
+ { id: 11, isbg: true,  frames: ['cubetop4', 1] },
+ { id: 12, isbg: false,  frames: ['cubebot1', 1] },
+ { id: 13, isbg: false,  frames: ['cubebot2', 1] },
+ { id: 14, isbg: false,  frames: ['cubebot3', 1] },
+ { id: 15, isbg: false,  frames: ['cubebot4', 1] }
  ];
