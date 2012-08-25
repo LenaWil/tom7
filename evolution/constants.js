cf10ec679b4cffec3cbcfe0b461a45d63a6e7483
tom7 <tom7@cs.cmu.edu>
2012-08-25 15:55:53 +0000

    very basics...

---
+++ evolution/constants.js
@@ -0,0 +1,22 @@
+var SCREENWIDTH = 832;
+var SCREENHEIGHT = 700;
+
+// Game area width and height in screen pixels.
+var WIDTH = 832;
+var HEIGHT = 624;
+
+// Dimensions of the screen in tiles.
+var TILESW = 16;
+var TILESH = 12;
+
+// In game pixels (2x2).
+var TILEWIDTH = 26;
+var TILEHEIGHT = 26;
+
+// z depths, just used in flash.
+var BGIMAGEDEPTH = 290;
+
+// There are four world panels loaded at any given time,
+// which occupy these depths.
+var WORLDDEPTHS = 300; // - 304
+
---
+++ evolution/gencrop.cc
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+
+#define TILEWIDTH 26
+#define TILEHEIGHT 26
+#define TILESW 16
+#define TILESH 12
+
+#define WORLDTILES 384
+
+#define SCREENWIDTH (TILEWIDTH * TILESW)
+#define SCREENHEIGHT (TILEHEIGHT * TILESH)
+
+int main () {
+
+  printf("default :");
+  for (int screeny = 0; screeny < (WORLDTILES / TILESH); screeny++) {
+    for (int screenx = 0; screenx < (WORLDTILES / TILESW); screenx++) {
+      printf(" screen_%d_%d.png", screenx, screeny);
+    }
+  }
+  printf("\n\n");
+
+  for (int screeny = 0; screeny < (WORLDTILES / TILESH); screeny++) {
+    for (int screenx = 0; screenx < (WORLDTILES / TILESW); screenx++) {
+      printf("screen_%d_%d.png : ../wholemap.gif\n"
+	     "\t/bin/convert ../wholemap.gif -crop %dx%d+%d+%d\\! $@\n\n",
+	     screenx, screeny,
+	     SCREENWIDTH, SCREENHEIGHT,
+	     SCREENWIDTH * screenx, SCREENHEIGHT * screeny);
+    }
+  }
+
+  return 0;
+}
