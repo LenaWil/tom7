24ceac0d9fab512a29a86ef05c45cd8a646893e8
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2012-11-02 20:37:35 +0000

    Start on MidiMusicLayer. But maybe more important to do some graphics first

--- trunk/synthtest/music-layer.h
+++ trunk/synthtest/music-layer.h
@@ -18,24 +18,16 @@ struct MusicLayer {
 
   // If this is a left-finite layer, return true and set the argument
   // to the first music of the clip. If left-infinite, return false.
-  virtual bool FirstMusic(int64 *t);
+  virtual bool FirstSample(int64 *t) = 0;
   // If this is a right-finite layer, return true and set the argument
   // to one past the last music of the clip.
-  virtual bool AfterLastMusic(int64 *t);
+  virtual bool AfterLastSample(int64 *t) = 0;
 
   // Compute the music at the given time. Must only be called
   // for finite clips when this is within the bounds of the clip.
   // The layer is responsible for doing its own caching and
   // maintenance of dependents that may have become dirty.
-  virtual double MusicAt(int64 t) = 0;
-
-  // TODO: Something to describe which regions have changed since
-  // some iteration (we need some kind of global clock that
-  // advances with any change, so that we can ask: Are musics
-  // I read from you at iteration i still valid?)
-  // For example, it could cover all of time with a series of
-  // intervals, which describe the last-changed time?
-
+  virtual vector<Controllers> NoteAt(int64 t) = 0;
 };
 
 #endif
