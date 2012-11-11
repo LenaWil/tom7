7444b3481a6818443876dd929c6007212cbd1116
tom7 <tom7@cs.cmu.edu>
2012-11-11 19:26:26 +0000

    Initial import. compiles headless on win64

---
+++ tasbot/fceu/drivers/dummy/dummy.h
@@ -0,0 +1,16 @@
+#ifndef __FCEU_DUMMY_H
+#define __FCEU_DUMMY_H
+
+// #include <DUMMY.h>
+#include "main.h"
+#include "dface.h"
+#include "input.h"
+
+static void DoFun(int frameskip);
+static int isloaded = 0;
+extern int noGui;
+
+int LoadGame(const char *path);
+int CloseGame(void);
+
+#endif
