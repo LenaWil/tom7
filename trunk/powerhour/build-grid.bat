af078bd18bd55c6c0c65f8fce0a730acbdad85ba
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2012-11-15 05:03:45 +0000

    grid making

---
+++ trunk/powerhour/build-grid.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\MLton\bin\mlton" -target x86_64-w64-mingw32 -codegen amd64 grid.cm
---
+++ trunk/powerhour/build.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\MLton\bin\mlton" -target x86_64-w64-mingw32 -codegen amd64 powerhour.cm
+
+
