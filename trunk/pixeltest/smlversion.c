b62d33368c9a14ea01baf63e6069f62c85bf7295
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2013-01-05 23:32:09 +0000

    SML version is actually faster, yay

--- trunk/pixeltest/smlversion.c
+++ trunk/pixeltest/smlversion.c
@@ -6,34 +6,8 @@ int counter = 0;
 SDL_Surface *screen;
 
 static SDL_Surface *MakeScreen(int w, int h) {
-
-  /* Can't use HWSURFACE here, because not handling this SDL_BlitSurface
-     case mentioned in the documentation:
-
-     "If either of the surfaces were in video memory, and the blit returns -2,
-     the video memory was lost, so it should be reloaded with artwork
-     and re-blitted."
-
-     Plus, on Windows, the only time you get HWSURFACE is with FULLSCREEN.
-
-     -- Adam
-  */
-
-  /* SDL_ANYFORMAT
-     "Normally, if a video surface of the requested bits-per-pixel (bpp)
-     is not available, SDL will emulate one with a shadow surface.
-     Passing SDL_ANYFORMAT prevents this and causes SDL to use the
-     video surface, regardless of its pixel depth."
-
-     Probably should not pass this.
-
-     -- Adam
-  */
-
-
-  /* SDL_DOUBLEBUF only valid with SDL_HWSURFACE! */
   SDL_Surface * ret = SDL_SetVideoMode(w, h, 32,
-                                       SDL_SWSURFACE /* | SDL_RESIZABLE */);
+                                       SDL_SWSURFACE);
   return ret;
 }
 
@@ -42,66 +16,23 @@ static void ClearSurface(SDL_Surface *s, Uint32 color) {
 }
 
 typedef unsigned char uint8;
-static uint8 ii = 0, jj = 0;
-static uint8 ss[256];
 
 void InitGame() {
   screen = MakeScreen(WIDTH * PIXELSIZE, HEIGHT * PIXELSIZE);
   ClearSurface(screen, 1234567);
   SDL_Flip(screen);
-
-  static const uint8 kk[256] = "--------------------a-3--------------------e-------------------------------e-e----------------------------------f----------------------------------------------z-----------------------------------------------------------------------------------------000----";
-  for (int i = 0; i < 256; i++) {
-    ss[i] = i;
-  }
-  uint8 i = 0, j = 0;
-  for (int n = 256; n--;) {
-    j += ss[i] + kk[i];
-    uint8 t = ss[i];
-    ss[i] = ss[j];
-    ss[j] = t;
-    i++;
-  }
 }
 
-static uint8 Byte() {
-  ii++;
-  jj += ss[ii];
-  uint8 ti = ss[ii];
-  uint8 tj = ss[jj];
-  ss[ii] = tj;
-  ss[jj] = ti;
-
-  return ss[(ti + tj) & 255];
-}
-
-// XXX locking?
-void FillScreen2x() {
+void FillScreenFrom(Uint32 *inpixels) {
   SDL_Surface *surf = screen;
-  static Sint64 r = 0x50, g = 0x80, b = 0xFE, a = 0xFF, f = 0xFFFFFF;
   // printf("RGB %d %d %d\n", R, G, B);
   CHECK(surf->format->BytesPerPixel == 4);
+  // CHECK(inpixels[0] == 0xFFAAAAAA);
 
+  // PERF Worth experimenting with different ways of pipelining this.
   for (int y = 0; y < HEIGHT; y++) {
     for (int x = 0; x < WIDTH; x++) {
-#if 0
-      f = (f * 67) & 0xFFFFFFFF;
-      f = f * 156;
-      f += x;
-      f = f & 0xFFFFFF;
-
-      r *= 25;
-      r += (x + counter / 32);
-      g++;
-      g *= (y + counter);
-      b *= 31337;
-      b += f;
-      f = r ^ g;
-#endif
-      uint8 r = Byte(), g = Byte(), b = Byte();
-
-      // PERF?
-      Uint32 color = SDL_MapRGB(surf->format, r, g, b);
+      Uint32 color = inpixels[y * WIDTH + x];
       Uint32 *pixels = (Uint32 *)surf->pixels;
 
       // pixel offset of top-left corner of megapixel
--- trunk/pixeltest/smlversion.sml
+++ trunk/pixeltest/smlversion.sml
@@ -3,14 +3,32 @@ val () = print "Hello.\n";
 
 exception Quit
 
+val WIDTH = 320
+val HEIGHT = 200
+
 local
     (* If you get link errors about __imp, it's probably because you
        didn't import with "private" (or "public") visibility. It seems
        the default is now to expect a DLL to provide the symbol, which
        is what __imp is about. *)
     val init = _import "InitGame" private : unit -> unit ;
-    val fs = _import "FillScreen2x" private : unit -> unit ;
+    val fillscreen = _import "FillScreenFrom" private : Word32.word Array.array -> unit ;
     val ctr = ref 0
+
+    val pixels = Array.array(WIDTH * HEIGHT, 0wxFFAAAAAA : Word32.word)
+
+    val rc = ARCFOUR.initstring "anything"
+
+    local
+        val orb = Word32.orb
+        infix orb
+    in
+        fun mixcolor (r, g, b, a) =
+            Word32.<< (a, 0w24) orb
+            Word32.<< (b, 0w16) orb
+            Word32.<< (g, 0w8) orb
+            r
+    end
 in
     val _ = init ()
     val start = Time.now()
@@ -21,10 +39,29 @@ in
           | SOME (SDL.E_KeyDown { sym = SDL.SDLK_ESCAPE }) => raise Quit
           | _ => ()
 
+    fun randomize () =
+        Util.for 0 (HEIGHT - 1)
+        (fn y =>
+         Util.for 0 (WIDTH - 1)
+         (fn x =>
+          let
+              fun byte () = Word32.fromInt (Word8.toInt (ARCFOUR.byte rc))
+              val r = byte ()
+              val g = byte ()
+              val b = byte ()
+              val a = 0wxFF : Word32.word
+              val color = mixcolor (r, g, b, a)
+          in
+              Array.update(pixels, y * WIDTH + x, color)
+          end
+          ))
+
     fun loop () =
         let
             val () = keypress ()
-            val () = fs ()
+
+            val () = randomize ()
+            val () = fillscreen pixels
             val () = ctr := !ctr + 1
         in
             if !ctr mod 1000 = 0
@@ -34,7 +71,7 @@ in
                     val sec = Time.toSeconds (Time.-(now, start))
                     val fps = real (!ctr) / Real.fromLargeInt (sec)
                 in
-                    print (LargeInt.toString sec ^ " (" ^
+                    print (Int.toString (!ctr) ^ " (" ^
                            Real.fmt (StringCvt.FIX (SOME 2)) fps ^ ")\n")
                 end
             else ();
