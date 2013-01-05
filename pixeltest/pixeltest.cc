380be8666e4b7606bd7614e0e6854b1aca9a67db
tom7 <tom7@cs.cmu.edu>
2013-01-05 19:06:27 +0000

    Compiles and runs

--- pixeltest/pixeltest.cc
+++ pixeltest/pixeltest.cc
@@ -1,60 +1,8 @@
 
-#include "10goto20.h"
-#include "audio-engine.h"
-
-#include "bleep-bloop-sample-layer.h"
-
-#include "jdksmidi/world.h"
-#include "jdksmidi/track.h"
-#include "jdksmidi/multitrack.h"
-#include "jdksmidi/filereadmultitrack.h"
-#include "jdksmidi/fileread.h"
-#include "jdksmidi/fileshow.h"
+#include "pixeltest.h"
 
 #define STARTW 800
 #define STARTH 600
-
-static SampleLayer *layer;
-
-int64 ttt = 0LL;
-
-// XXX dithering, etc.
-static Sint16 DoubleTo16(double d) {
-  if (d > 1.0) return 32767;
-  // signed 16-bit int goes to -32768, but we never use
-  // this sample value; symmetric amplitudes seem like
-  // the right thing?
-  else if (d < -1.0) return -32767;
-  else return (Sint16)(d * 32767.0);
-}
-
-void Audio(void *userdata, Uint8 *stream_bytes, int num_bytes) {
-  Sint16 *stream = (Sint16*) stream_bytes;
-  int samples = num_bytes / sizeof (Sint16);
-
-  // Two channels.
-  for (int i = 0; i < samples; i += 2) {
-    Sample s = layer->SampleAt(ttt);
-    stream[i] = DoubleTo16(s.left);
-    stream[i + 1] = DoubleTo16(s.right);
-    ttt++;
-  }
-}
-
-int waste(void *unused) {
-  int x = 0;
-  for(;;) {
-    x *= 0xBEEF;
-    x ^= 0xDEAD;
-    if (x & 1) {
-      x <<= 1;
-    } else {
-      x >>= 1;
-    }
-  }
-  
-  return -1;
-}
 
 int main (int argc, char **argv) {
 
@@ -63,20 +11,18 @@ int main (int argc, char **argv) {
     abort();
   }
 
-  fprintf(stderr, "10 GOTO 20\n");
+  fprintf(stderr, "PIXEL TEST\n");
 
   //  MidiMusicLayer *midi = MidiMusicLayer::Create("sensations.mid");
   //   CHECK(midi);
 // XXX use it...
-
-  layer = BleepBloopSampleLayer::Create();
-  CHECK(layer);
 
   SDL_EnableUNICODE(1);
   SDL_Surface *screen = sdlutil::makescreen(STARTW, STARTH);
   sdlutil::clearsurface(screen, 1234567);
   SDL_Flip(screen);
 
+#if 0
   SDL_AudioSpec spec, obtained;
   spec.freq = SAMPLINGRATE;
   spec.samples = 2048;
@@ -96,6 +42,7 @@ int main (int argc, char **argv) {
   SDL_Thread *t4 = SDL_CreateThread(waste, 0);
   SDL_Thread *t5 = SDL_CreateThread(waste, 0);
   SDL_Thread *t6 = SDL_CreateThread(waste, 0);
+#endif
 
   while(1) {
     SDL_Event e;
