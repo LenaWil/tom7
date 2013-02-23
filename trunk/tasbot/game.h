bafcb8365c33f3356068c195fd39d137a3f9a881
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2013-02-23 17:52:41 +0000

    Some progress on backtracking. need to write helper part

--- trunk/tasbot/game.h
+++ trunk/tasbot/game.h
@@ -5,6 +5,7 @@
 #define GAME "mario"
 #define MOVIE "mario-cleantom.fm2"
 #define FASTFORWARD 256  // XXX cheats! -- should be 0
+#define BASE64 "base64:jjYwGG411HcjG/j9UOVM3Q=="
 
 // #define GAME "pacman"
 // #define MOVIE "pacman-tom.fm2"
