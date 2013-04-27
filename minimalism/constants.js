e71dab8772639ce47d2616e8c7b5927578877e8d
tom7 <tom7@cs.cmu.edu>
2013-04-27 14:27:08 +0000

    Drawing very basic windows

---
+++ minimalism/constants.js
@@ -0,0 +1,9 @@
+
+var CORNER = 23;
+var BORDER = 4;
+var TITLE = 19;
+var TOOL = 20;
+
+var GREY = '#c3c7cb'; // ???
+var BLUE = '#00a';
+
---
+++ minimalism/jsutil.js
@@ -0,0 +1,35 @@
+
+function DomMaker(ty) {
+  return function(cl, par) {
+    var elt = document.createElement(ty);
+    if (cl) {
+      elt.setAttribute('class', cl);
+      elt.setAttribute('className', cl);
+    }
+    par && par.appendChild(elt);
+    return elt;
+  };
+}
+var DIV = DomMaker('DIV');
+var SPAN = DomMaker('SPAN');
+var A = DomMaker('A');
+var INPUT = DomMaker('INPUT');
+var TABLE = DomMaker('TABLE');
+var TBODY = DomMaker('TBODY');
+var TR = DomMaker('TR');
+var TD = DomMaker('TD');
+var TH = DomMaker('TH');
+var BR = DomMaker('BR');
+var IMG = DomMaker('IMG');
+var SCRIPT = DomMaker('SCRIPT');
+
+function TEXT(s, par) {
+  var elt = document.createTextNode(s);
+  par && par.appendChild(elt);
+  return elt;
+}
+
+
+function px(i) {
+  return '' + i + 'px';
+}
