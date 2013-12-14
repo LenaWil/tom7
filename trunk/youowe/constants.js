1367589c5297144960e5ded1a3ed866b477b3fd4
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2013-12-14 16:32:33 +0000

    Frames

--- trunk/youowe/constants.js
+++ trunk/youowe/constants.js
@@ -4,3 +4,8 @@ var DEBUG = true;
 var WIDTH = 320;
 var HEIGHT = 200;
 var PX = 4;
+
+// ?
+var TOP = 24;
+
+// Registration is to the bottom center of the player.
--- trunk/youowe/game.js
+++ trunk/youowe/game.js
@@ -2,8 +2,10 @@
 var counter = 0;
 var start_time = (new Date()).getTime();
 
+// Number of elapsed frames in the current scene.
+var frames = 0;
 
-var PHASE_TITLE = 0;
+var PHASE_TITLE = 0, PHASE_PLAYING = 1;
 var phase = PHASE_TITLE;
 
 var images = new Images(
@@ -16,35 +18,166 @@ var images = new Images(
 
 function SetPhase(p) {
   frames = 0;
+  // XXX clear other state
+
   phase = p;
 }
 
-function TitleFrame(time) {
-  ctx.drawImage(images.Get('title.png'), 0, 0);
-  // XXX handle keys?
+var holdingLeft = false, holdingRight = false,
+  holdingUp = false, holdingDown = false,
+  holdingSpace = false, holdingEnter = false;
+
+document.onkeydown = function(e) {
+  e = e || window.event;
+  if (e.ctrlKey) return true;
+
+  switch (e.keyCode) {
+    case 37: // LEFT
+    holdingLeft = true;
+    break;
+    case 38: // UP
+    holdingUp = true;
+    break;
+    case 39: // RIGHT
+    holdingRight = true;
+    break;
+    case 40: // DOWN
+    holdingDown = true;
+    break;
+    case 32: // SPACE
+    holdingSpace = true;
+    break;
+    case 13: // ENTER
+    holdingEnter = true;
+    break;
+    // TODO z, x, enter
+  } 
+  var elt = document.getElementById('key');
+  elt && (elt.innerHTML = 'key: ' + e.keyCode);
+  return false;
 }
 
-var frames = 0;
-function Frame(time) {
+document.onkeyup = function(e) {
+  e = e || window.event;
+  if (e.ctrlKey) return true;
+
+  switch (e.keyCode) {
+    case 37: // LEFT
+    holdingLeft = false;
+    break;
+    case 38: // UP
+    holdingUp = false;
+    break;
+    case 39: // RIGHT
+    holdingRight = false;
+    break;
+    case 40: // DOWN
+    holdingDown = false;
+    break;
+    case 32: // SPACE
+    holdingSpace = false;
+    break;
+    case 13: // ENTER
+    holdingEnter = false;
+    break;
+  }
+  return false;
+}
+
+function Frames(arg) {
+  if (arg instanceof Image) {
+    // Assume static.
+    this.frames = [{f: arg, d: 1}];
+  } else {
+    // Assume list.
+    this.frames = arg;
+  }
+  
+  var ct = 0;
+  for (var i = 0; i < this.frames.length; i++) {
+    ct += this.frames[i].d;
+  }
+  this.numframes = ct;
+
+  this.GetFrame = function() {
+    var f = frames % this.numframes;
+    for (var i = 0; i < this.frames.length; i++) {
+      if (f < this.frames[i].d) {
+	return this.frames[i].f;
+      }
+      f -= this.frames[i].d;
+    }
+    return this.frames[0].f;
+  };
+}
+
+function Static(f) { return new Frames(images.Get(f)); }
+function Room(bg) {
+  this.bg = bg;
+  this.width = bg.width;
+  // ...?
+}
+
+var rooms = {
+  classroom: new Room(Static('classroom.png'))
+};
+
+// Sets up the context 
+function WarpTo(roomname, x, y) {
+  currentroom = roomname;
+}
+
+function TitleStep(time) {
+  ctx.drawImage(images.Get('title.png'), 0, 0);
+  if (holdingEnter) {
+    WarpTo('classroom', 77, 116);
+    SetPhase(PHASE_PLAYING);
+  }
+}
+
+// XXX crop rectangle version?
+function DrawFrame(frame, x, y) {
+  ctx.drawImage(frame.GetFrame(), x, y);
+}
+
+function PlayingStep(time) {
+  // XXX Compute scrollx...
+  scrollx = 0;
+
+  // Redraw everything every frame (!)
+  ctx.fillStyle = "#000";
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+  DrawFrame(rooms[currentroom].bg, -scrollx, TOP);
+
+  // XXX Draw me (x, y + TOP)
+  
+}
+
+function Step(time) {
   // XXX here, throttle to 30 fps or something we
   // should be able to hit on most platforms.
   frames++;
+  if (frames > 1000000) frames = 0;
 
   if (phase == PHASE_TITLE) {
-    TitleFrame(time);
-  } else {
+    TitleStep(time);
+  } else if (phase == PHASE_PLAYING) {
+    PlayingStep(time);
+    
 
     /*
     // Put pixels
     id.data.set(buf8);
     ctx.putImageData(id, 0, 0);
     */
-
+    /*
     for (var i = 0; i < 1000; i++) {
       var x = 0 | ((f * i) % WIDTH);
       var y = 0 | ((f * 31337 + i) % HEIGHT);
       ctx.drawImage(eye, x - 16, y - 16);
     }
+    */
     
     // ...
   }
@@ -63,13 +196,13 @@ function Frame(time) {
   }
 
   // And continue the loop...
-  window.requestAnimationFrame(Frame);
+  window.requestAnimationFrame(Step);
 }
 
 function Start() {
   phase = PHASE_TITLE;
   start_time = (new Date()).getTime();
-  window.requestAnimationFrame(Frame);
+  window.requestAnimationFrame(Step);
 }
 
 images.WhenReady(Start);
