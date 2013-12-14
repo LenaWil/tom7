4969dc8a93b92e071cc963ec3c966a0f7a85295c
tom7 <tom7@cs.cmu.edu>
2013-12-14 23:45:01 +0000

    Do fear the reaper

--- youowe/constants.js
+++ youowe/constants.js
@@ -28,13 +28,22 @@ var TERMINAL_X = 4;
 var TERMINAL_Y = 2;
 var TERMINAL_Z = 8;
 
+// Fighting
 var ATTACKSIZE_Y = 6;
 var ATTACKSIZE_X = 14;
 var HALFPERSON_W = 8;
 
-// Fighting
 var PUNCHFRAMES = 5;
 var KICKFRAMES = 9;
+// Needs to be < PUNCHFRAMES - 2, or else
+// infinite punch combo is possible
+var HURTFRAMES = 2;
+var KOFRAMES = 40;
+var DEADFRAMES = 60;
+
+var PUNCH_DAMAGE = 2;
+var KICK_DAMAGE = 3;
+
 
 var FONTW = 9;
 var FONTH = 16;
