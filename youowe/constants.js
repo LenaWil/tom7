14a06f8e9edcc62c88b1a7b751e9c39fb07e9222
tom7 <tom7@cs.cmu.edu>
2013-12-14 18:27:59 +0000

    basic walking and animation of that, scrolling

--- youowe/constants.js
+++ youowe/constants.js
@@ -5,9 +5,18 @@ var WIDTH = 320;
 var HEIGHT = 200;
 var PX = 4;
 
+var GAMEHEIGHT = 128;
+
 var ME_HEIGHT = 32;
 
 // ?
 var TOP = 24;
 
 // Registration is to the bottom center of the player.
+
+
+// Physics
+var ACCEL_X = 0.4;
+var ACCEL_Y = 0.2;
+var TERMINAL_X = 4;
+var TERMINAL_Y = 2;
