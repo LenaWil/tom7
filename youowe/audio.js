c9475c54d8386b06e04129db4af692a725558eab
tom7 <tom7@cs.cmu.edu>
2013-12-15 14:07:35 +0000

    Move music to its own file and make it build automatically with makefile. Implement infinite looping for music, with the help of dumpmidi

--- youowe/audio.js
+++ youowe/audio.js
@@ -29,8 +29,9 @@ function StartSong(s) {
   for (var i = 0; i < song.length; i++) {
     var type = TYPE_SINE;
     switch (song[i].inst) {
-      case 'SAW': type = TYPE_SAW;
-      case 'SQUARE': type = TYPE_SQUARE;
+      case 'SAW': type = TYPE_SAW; break;
+      case 'SQUARE': type = TYPE_SQUARE; break;
+      case 'TRIANGLE': type = TYPE_TRI; break;
       default: TYPE_SINE;
     }
 
@@ -43,9 +44,7 @@ function StartSong(s) {
 		  notes: notes,
 		  idx: 0,
 		  active: [],
-		  basetime: now,
-		  });
-
+		  basetime: now });
   }
   
   UpdateSong(s);
@@ -56,7 +55,7 @@ function MidiFreq(m) {
 }
 
 function DoSoundEvent(tr, e, d) {
-  console.log('dosoundevent ' + e.on + ' ' + e.off + ' @' + d);
+  // console.log('dosoundevent ' + e.on + ' ' + e.off + ' @' + d);
   if ('on' in e) {
     if (tr.active[e.on]) {
       // Don't want to leak...
@@ -69,12 +68,12 @@ function DoSoundEvent(tr, e, d) {
     // 256 + 10 * e.on;
     src.frequency.value = MidiFreq(e.on);
     var gain = audioctx.createGain();
-    gain.gain.value = 0.33;
+    gain.gain.value = 0.275;
     src.connect(gain);
 
     var lowpass = audioctx.createBiquadFilter();
     lowpass.type = 0; // LOWPASS
-    lowpass.frequency.value = 8192;
+    lowpass.frequency.value = 4096; // 8192;
     gain.connect(lowpass);
     lowpass.connect(audioctx.destination);
 
@@ -85,9 +84,12 @@ function DoSoundEvent(tr, e, d) {
     var src = tr.active[e.off];
     delete tr.active[e.off];
     if (src) {
-      console.log('turn off at ' + (audioctx.currentTime + d));
+      // console.log('turn off at ' + (audioctx.currentTime + d));
       src.stop(audioctx.currentTime + d / 1000.0);
     }
+
+  } else if ('w' in e) {
+    // Nothing; just for alignment at the end.
 
   } else {
     console.log ('bad sound event');
@@ -101,7 +103,7 @@ function UpdateTrack(now, playuntil, tr) {
   // wall time against which the next event's delta
   // is measured.
 
-  while (tr.idx < tr.notes.length) {
+  for (;;) {
     var note = tr.notes[tr.idx];
     var playtime = tr.basetime + note.d;
     if (playtime <= playuntil) {
@@ -111,13 +113,16 @@ function UpdateTrack(now, playuntil, tr) {
       // If this note is not in the window then no note will be.
       // Basetime stays the same; we're still looking at the
       // same index.
-      return;
+      return true;
     }
 
     // Move on to the next note.
     tr.idx++;
+    if (tr.idx == tr.notes.length) tr.idx = 0;
     tr.basetime = playtime;
   }
+
+  // unreachable
 }
 
 var MSPERSCHEDULE = 100;
@@ -125,16 +130,11 @@ function UpdateSong() {
   var now = audioctx.currentTime * 1000.0; // (new Date()).getTime();
   var playuntil = now + MSPERSCHEDULE;
 
+  var played = false;
   // We have to run all the events with delta time
   // less than late immediately.
   for (var i = 0; i < tracks.length; i++) {
     // console.log(tracks[i]);
-    UpdateTrack(now, playuntil, tracks[i]);
-
-    // Do something when we finish the whole song, yeah?
+    played = UpdateTrack(now, playuntil, tracks[i]) || played;
   }
 }
-
-var overworld = [{ inst: 'SAW', notes: [{d:0,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:33},{d:30,off:33},{d:30,on:33},{d:30,off:33},{d:30,on:33},{d:30,off:33},{d:30,on:45},{d:30,off:45},{d:30,on:33},{d:30,off:33},{d:30,on:33},{d:30,off:33},{d:30,on:33},{d:30,off:33},{d:30,on:45},{d:30,off:45},{d:30,on:29},{d:30,off:29},{d:30,on:29},{d:30,off:29},{d:30,on:29},{d:30,off:29},{d:30,on:52},{d:30,off:52},{d:30,on:29},{d:30,off:29},{d:30,on:29},{d:30,off:29},{d:30,on:29},{d:30,off:29},{d:30,on:52},{d:30,off:52},{d:30,on:24},{d:30,off:24},{d:30,on:24},{d:30,off:24},{d:30,on:24},{d:30,off:24},{d:30,on:48},{d:30,off:48},{d:30,on:24},{d:30,off:24},{d:30,on:24},{d:30,off:24},{d:30,on:24},{d:30,off:24},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:33},{d:30,off:33},{d:30,on:33},{d:30,off:33},{d:30,on:33},{d:30,off:33},{d:30,on:45},{d:30,off:45},{d:30,on:33},{d:30,off:33},{d:30,on:33},{d:30,off:33},{d:30,on:33},{d:30,off:33},{d:30,on:45},{d:30,off:45},{d:30,on:29},{d:30,off:29},{d:30,on:29},{d:30,off:29},{d:30,on:29},{d:30,off:29},{d:30,on:53},{d:30,off:53},{d:30,on:29},{d:30,off:29},{d:30,on:29},{d:30,off:29},{d:30,on:29},{d:30,off:29},{d:30,on:53},{d:30,off:53},{d:30,on:31},{d:30,off:31},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:36},{d:30,off:36},{d:30,on:48},{d:30,off:48},{d:30,on:43},{d:30,off:43},{d:30,on:43},{d:30,off:43},{d:30,on:43},{d:30,off:43},{d:30,on:55},{d:30,off:55},{d:30,on:43},{d:30,off:43},{d:30,on:43},{d:30,off:43},{d:30,on:43},{d:30,off:43}]},
-{ inst: 'SQUARE', notes: [{d:960,on:52},{d:135,off:52},{d:165,on:55},{d:90,off:55},{d:30,on:50},{d:135,off:50},{d:225,on:57},{d:90,off:57},{d:30,on:52},{d:180,off:52},{d:840,on:52},{d:135,off:52},{d:165,on:55},{d:90,off:55},{d:30,on:50},{d:135,off:50},{d:225,on:57},{d:90,off:57},{d:30,on:52},{d:180,off:52},{d:600,on:52},{d:30,off:52},{d:0,on:50},{d:30,off:50},{d:0,on:48},{d:30,off:48},{d:30,on:50},{d:30,off:50},{d:30,on:45},{d:120,off:45},{d:240,on:48},{d:60,off:48},{d:0,on:50},{d:60,off:50},{d:0,on:45},{d:120,off:45},{d:240,on:45},{d:60,off:45},{d:0,on:48},{d:60,off:48},{d:0,on:40},{d:120,off:40},{d:660,on:60},{d:0,on:52},{d:30,off:52},{d:0,off:60},{d:0,on:59},{d:0,on:50},{d:30,off:50},{d:0,off:59},{d:0,on:57},{d:0,on:48},{d:30,off:48},{d:0,off:57},{d:30,on:64},{d:0,on:50},{d:30,off:50},{d:0,off:64},{d:30,on:55},{d:0,on:45},{d:150,off:45},{d:30,off:55},{d:0,on:57},{d:60,off:57},{d:0,on:60},{d:120,off:60},{d:0,on:55},{d:60,off:55},{d:0,on:57},{d:60,off:57},{d:0,on:41},{d:0,on:64},{d:120,off:41},{d:30,off:64},{d:30,on:64},{d:60,off:64},{d:0,on:60},{d:30,off:60},{d:0,on:62},{d:60,off:62},{d:0,on:55},{d:30,off:55},{d:0,on:57},{d:60,off:57},{d:0,on:60},{d:60,off:60},{d:0,on:52},{d:120,off:52},{d:0,on:50},{d:60,off:50},{d:0,on:55},{d:180,off:55},{d:0,on:57},{d:60,off:57},{d:0,on:60},{d:0,on:52},{d:60,off:60},{d:0,on:61},{d:15,off:61},{d:0,on:62},{d:165,off:62},{d:0,off:52},{d:240,on:64},{d:60,off:64},{d:0,on:62},{d:180,off:62},{d:0,on:64},{d:60,off:64},{d:0,on:57},{d:120,on:53},{d:60,off:53},{d:0,off:57},{d:0,on:64},{d:60,off:64},{d:0,on:62},{d:180,off:62},{d:0,on:64},{d:60,off:64},{d:0,on:53},{d:0,on:57},{d:180,off:57},{d:0,off:53},{d:0,on:55},{d:60,off:55},{d:0,on:60},{d:0,on:64},{d:180,off:64},{d:0,off:60},{d:0,on:55},{d:180,off:55},{d:0,on:57},{d:60,off:57},{d:0,on:52},{d:0,on:60},{d:60,off:60},{d:0,on:61},{d:15,off:61},{d:0,on:62},{d:165,off:62},{d:0,off:52}]}];
-
