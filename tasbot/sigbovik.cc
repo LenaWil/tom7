9b30822c08938e7d7842fb14d9fa15adf06dde2c
tom7 <tom7@cs.cmu.edu>
2014-04-01 20:14:15 +0000

    sync sigbovik for conference

--- tasbot/sigbovik.cc
+++ tasbot/sigbovik.cc
@@ -27,6 +27,7 @@
 #include "sdlutil-lite.h"
 #include "util.h"
 #include "../cc-lib/textsvg.h"
+#include "../cc-lib/stb_image.h"
 
 #include "SDL.h"
 #include "SDL_net.h"
@@ -34,7 +35,12 @@
 #include "netutil.h"
 using ::google::protobuf::Message;
 
-#define WIDTH 256
+#define BOVIKWIDTH 186
+#define HEADHEIGHT 70
+#define FEETY 163
+#define DIGITWIDTH 10
+
+#define WIDTH (256 + BOVIKWIDTH)
 #define HEIGHT 256
 SDL_Surface *screen = 0;
 
@@ -43,13 +49,51 @@ SDL_Surface *screen = 0;
 
 // assumes RGBA, surfaces exactly the same size, etc.
 static void CopyRGBA(const vector<uint8> &rgba, SDL_Surface *surface) {
-  int bpp = surface->format->BytesPerPixel;
+  // int bpp = surface->format->BytesPerPixel;
   Uint8 * p = (Uint8 *)surface->pixels;
   memcpy(p, &rgba[0], surface->w * surface->h * 4);
 }
 
+struct Graphic {
+  // From a PNG file.
+  explicit Graphic(const string &filename) {
+    int bpp;
+    uint8 *stb_rgba = stbi_load(filename.c_str(), 
+				&width, &height, &bpp, 4);
+    CHECK(stb_rgba);
+    for (int i = 0; i < width * height * 4; i++) {
+      rgba.push_back(stb_rgba[i]);
+      //       if (bpp == 3 && (i % 3 == 2)) {
+      // rgba.push_back(0xFF);
+      //}
+    }
+    stbi_image_free(stb_rgba);
+    fprintf(stderr, "%s is %dx%d @%dbpp = %d bytes\n",
+	    filename.c_str(), width, height, bpp,
+	    rgba.size());
+
+    surf = sdlutil::makesurface(width, height, true);
+    CHECK(surf);
+    CopyRGBA(rgba, surf);
+  }
+
+  // to screen
+  void Blit(int x, int y) {
+    sdlutil::blitall(surf, screen, x, y);
+  }
+
+  int width, height;
+  vector<uint8> rgba;
+  SDL_Surface *surf;
+};
+
 struct Sigbovik {
+  vector<Graphic> heads, feet, bodies, digits;
+
   Sigbovik() : log(NULL), rc("sigbovik") {
+    screen = sdlutil::makescreen(WIDTH, HEIGHT);
+    CHECK(screen);
+
     map<string, string> config = Util::ReadFileToMap("sigbovik-config.txt");
     if (config.empty()) {
       fprintf(stderr, "You need a file called config.txt; please "
@@ -57,13 +101,24 @@ struct Sigbovik {
       abort();
     }
 
+    for (int i = 0; i < 8; i++) {
+      string filename = StringPrintf("head%d.png", i);
+      heads.push_back(Graphic(filename));
+      string feetname = StringPrintf("feet%d.png", i);
+      feet.push_back(Graphic(feetname));
+      string bodyname = StringPrintf("body%d.png", i);
+      bodies.push_back(Graphic(bodyname));
+    }
+
+    for (int i = 0; i < 10; i++) {
+      string name = StringPrintf("digit%d.png", i);
+      digits.push_back(Graphic(name));
+    }
+
     game = config["game"];
     // const string moviename = config["movie"];
     CHECK(!game.empty());
     // CHECK(!moviename.empty());
-
-    screen = sdlutil::makescreen(WIDTH, HEIGHT);
-    CHECK(screen);
 
     Emulator::Initialize(game + ".nes");
 
@@ -91,7 +146,11 @@ struct Sigbovik {
   void Play() {
     // XXX inputs from web thing
     SDL_Surface *surf = sdlutil::makesurface(256, 256, true);
+    uint8 headbyte = 0, feetbyte = 171, bodybyte = 41,
+      mem1 = 0, mem2 = 0;
+    int frame = 0;
     for (;;) {
+      frame++;
       // Ignore events for now
       SDL_Event event;
       SDL_PollEvent(&event);
@@ -99,16 +158,63 @@ struct Sigbovik {
       case SDL_QUIT:
 	return;
       }
-
-      Emulator::Step(rc.Byte());
+      
+      SDL_Delay(1000.0 / 60.0);
+      
+      string votefile = Util::ReadFile("current_vote");
+      int current_vote = atoi(votefile.c_str());
+      uint8 input = 0;
+      if (current_vote > 0) {
+	input = 1 << (current_vote - 1);
+      }
+      printf("Current input: %s\n", SimpleFM2::InputToString(input).c_str());
+      Emulator::Step(input);
 
       vector<uint8> image;
       Emulator::GetImage(&image);
       CopyRGBA(image, surf);
+
+      sdlutil::clearsurface(screen, 0xFFFFFFFF);
+
       // Draw pixels to screen...
-      sdlutil::blitall(surf, screen, 0, 0);
+      sdlutil::blitall(surf, screen, BOVIKWIDTH, 0);
+
+      // and sigbovik person
+      // XXX get from RAM
+      // XXX change update rate sometimes
+      if (frame % 20 == 0) {
+
+	vector<uint8> mem;
+	Emulator::GetMemory(&mem);
+	headbyte = mem[0xdc];
+	bodybyte = mem[0x34];
+	feetbyte = mem[0x308] + mem[0x1f6] + mem[0x1d4];
+
+	mem1 = mem[0xef];
+	mem2 = mem[0xf0];
+      }
+
+      Graphic &head = heads[headbyte & 7];
+      Graphic &foot = feet[feetbyte & 7];
+      Graphic &body = bodies[bodybyte & 7];
+      head.Blit(0, 0);
+      body.Blit(0, 0);
+      foot.Blit(0, FEETY);
+
+      DrawNumber(mem1, 0, 0);
+      DrawNumber(mem2, 60, 0);
+      
       SDL_Flip(screen);
     }
+  }
+
+  void DrawNumber(uint8 n, int x, int y) {
+    int d1 = n / 100;
+    int d2 = (n % 100) / 10;
+    int d3 = n % 10;
+    digits[d1].Blit(x, y);
+    digits[d2].Blit(x + DIGITWIDTH, y);
+    digits[d3].Blit(x + DIGITWIDTH + DIGITWIDTH, y);
   }
 
   // Used to ffwd to gameplay.
