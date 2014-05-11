4d0efe95fb96c84d67b96f30df9874eef742041f
tom7 <tom7@cs.cmu.edu>
2014-05-11 14:30:15 +0000

    So much cleaner

--- arst/script_test.cc
+++ arst/script_test.cc
@@ -1,7 +1,16 @@
 
 #include "script.h"
+#include "SDL.h"
 
-int main() {
-  Script 
-
+int SDL_main(int argc, char *argv[]) {
+  Script *s = Script::FromString
+    ("0 hello\n"
+     "40 in\n"
+     "50 a\n"
+     "66 galaxy\n"
+     "77 far\n");
+  
+  s->DebugPrint();
+  delete s;
+  return 0;
 }
