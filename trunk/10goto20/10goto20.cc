274728a7eced265551ddea4ab0c0feb06e056af4
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2014-06-21 22:09:46 +0000

    Refactor a bit. Some progress on the audio engine. Document

--- trunk/10goto20/10goto20.cc
+++ trunk/10goto20/10goto20.cc
@@ -14,25 +14,7 @@
 #define STARTW 800
 #define STARTH 600
 
-#define AUDIOBUFFER 1024
-
-// XXX this stuff needs to go into audio-engine.
 static SampleLayer *layer;
-
-int64 ttt = 0LL;
-
-void Audio(void *userdata, Uint8 *stream_bytes, int num_bytes) {
-  Sint16 *stream = (Sint16*) stream_bytes;
-  int samples = num_bytes / sizeof (Sint16);
-
-  // Two channels.
-  for (int i = 0; i < samples; i += 2) {
-    Sample s = layer->SampleAt(ttt);
-    stream[i] = DoubleTo16(s.left);
-    stream[i + 1] = DoubleTo16(s.right);
-    ttt++;
-  }
-}
 
 // TODO: much better clamping, please! 
 struct SorryLayer : public SampleLayer {
@@ -52,6 +34,7 @@ struct SorryLayer : public SampleLayer {
 
 int main (int argc, char **argv) {
 
+  // and thread?
   if (SDL_Init (SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_TIMER) < 0) {
     fprintf(stderr, "Unable to init SDL: %s\n", SDL_GetError());
     abort();
@@ -85,24 +68,12 @@ int main (int argc, char **argv) {
     printf("Wrote wave.\n");
   }
 
+  AudioEngine::Init();
+
   SDL_EnableUNICODE(1);
   SDL_Surface *screen = sdlutil::makescreen(STARTW, STARTH);
   sdlutil::clearsurface(screen, 1234567);
   SDL_Flip(screen);
-
-  SDL_AudioSpec spec, obtained;
-  spec.freq = SAMPLINGRATE;
-  spec.samples = AUDIOBUFFER;
-  spec.channels = 2;
-  spec.callback = &Audio;
-  spec.userdata = 0;
-  spec.format = AUDIO_S16SYS;
-  SDL_OpenAudio(&spec, &obtained);
-
-  fprintf(stderr, "Audio started: %d Hz %d buffer\n",
-	  obtained.freq, obtained.samples);
-
-  SDL_PauseAudio(0);
 
   while(1) {
     SDL_Event e;
--- trunk/10goto20/audio-engine.h
+++ trunk/10goto20/audio-engine.h
@@ -1,5 +1,5 @@
 
-/* This is the basic audio rendering and output engine.
+/* This is the central audio rendering and output engine.
 
    There is just one global audio engine, since there's one song and
    one pair of speakers.
@@ -16,26 +16,37 @@ struct AudioEngine {
 
   static void Init();
 
-  // There is a single playback cursor, which is always within
-  // the array of samples. The engine can move and stop the cursor.
-  // Nominally, the song begins at sample 0, but it is possible
-  // for layers to extend before the song, so sample indices are
-  // signed.
+  // There is a single playback cursor, which is always within the
+  // array of samples (or one past its end). The engine can move and
+  // stop the cursor. Nominally, the song begins at sample 0, but it
+  // is possible for layers to extend before the song, so sample
+  // indices are signed. The cursor indicates the next sample to play.
   static void SetCursor(int64 sample);
   static int64 GetCursor();
+
+  // Replace the current song with the argument. Returns the
+  // current song. Takes ownership of the argument and relinquishes
+  // ownership of the return value, which btw may be null.
+  static SampleLayer *SwapLayer(SampleLayer *s);
+
+  // Mark the first sample index not in the song. Must be
+  // non-negative, since it must not be before the beginning point,
+  // which is always 0.
+  static void SetEnd(int64 end);
 
   // Whether output is occurring. The audio engine pauses itself if
   // the playback cursor reaches unrendered audio, so the display
   // engine should be checking this.
   static bool Playing();
-  // Set the playing state. Audio can pause itself at any time.
+  // Set the playing state. Audio can pause itself at any time,
+  // including immediately after a call to Play (essentially
+  // rejecting the call.)
   static void Play(bool play);
 
  private:
-  static SDL_mutex *mutex;
-  static int64 cursor;
-  static bool playing;
-  static SampleLayer *song;
+  // All static.
+  AudioEngine() = delete;
+  AudioEngine(const AudioEngine &) = delete;
 };
 
 #endif
