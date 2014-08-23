870d3e8ab0c389877543a8122a9d3631bb12acac
tom7 <tom7@cs.cmu.edu>
2014-08-23 02:03:15 +0000

    Starter code for Connector World

---
+++ connector/constants.js
@@ -0,0 +1,14 @@
+
+var DEBUG = false; // true;
+
+var WIDTH = 320;
+var HEIGHT = 200;
+var PX = 3;
+var MINFRAMEMS = 25.0;
+
+var FONTW = 9;
+var FONTH = 16;
+var FONTOVERLAP = 1;
+var FONTCHARS = " ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+    "abcdefghijklmnopqrstuvwxyz0123456789`-=" +
+    "[]\\;',./~!@#$%^&*()_+{}|:\"<>?";
