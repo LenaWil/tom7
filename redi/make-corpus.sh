f12ccfca8ade2e25432d067232c131fc522638aa
tom7 <tom7@cs.cmu.edu>
2015-02-16 20:52:52 +0000

    Reserve the corpus dir for the corpus itself. Add scraper

---
+++ redi/make-corpus.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+make fetcher.exe
+
+./fetcher.exe "moai statue"
+./fetcher.exe face
+./fetcher.exe cat
+./fetcher.exe dog
+./fetcher.exe computer
+./fetcher.exe sunset
+./fetcher.exe calculator
+./fetcher.exe sushi
+./fetcher.exe castle
+./fetcher.exe minivan
+./fetcher.exe surfing
+./fetcher.exe running
+./fetcher.exe icicle
+./fetcher.exe mushroom
+./fetcher.exe flower
+./fetcher.exe gecko
+./fetcher.exe owl
+./fetcher.exe tapir
+./fetcher.exe elephant
+./fetcher.exe swimming
+./fetcher.exe skiing
+./fetcher.exe stonehenge
+./fetcher.exe pyramids
