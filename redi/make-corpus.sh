d2b796a2888bd8fb4b77b7e4027cfd32cb463f8e
tom7 <tom7@cs.cmu.edu>
2015-02-28 23:12:34 +0000

    Add resampler, make some multithreaded stuff

--- redi/make-corpus.sh
+++ redi/make-corpus.sh
@@ -1,26 +1,81 @@
 #!/bin/sh
 make fetcher.exe
 
-./fetcher.exe "moai statue"
-./fetcher.exe face
-./fetcher.exe cat
-./fetcher.exe dog
-./fetcher.exe computer
-./fetcher.exe sunset
-./fetcher.exe calculator
-./fetcher.exe sushi
-./fetcher.exe castle
-./fetcher.exe minivan
-./fetcher.exe surfing
-./fetcher.exe running
-./fetcher.exe icicle
-./fetcher.exe mushroom
-./fetcher.exe flower
-./fetcher.exe gecko
-./fetcher.exe owl
-./fetcher.exe tapir
-./fetcher.exe elephant
-./fetcher.exe swimming
-./fetcher.exe skiing
-./fetcher.exe stonehenge
-./fetcher.exe pyramids
+# ./fetcher.exe "moai statue"
+# ./fetcher.exe face
+# ./fetcher.exe cat
+# ./fetcher.exe dog
+# ./fetcher.exe computer
+# ./fetcher.exe sunset
+# ./fetcher.exe calculator
+# ./fetcher.exe sushi
+# ./fetcher.exe castle
+# ./fetcher.exe minivan
+# ./fetcher.exe surfing
+# ./fetcher.exe running
+# ./fetcher.exe icicle
+# ./fetcher.exe mushroom
+# ./fetcher.exe flower
+# ./fetcher.exe gecko
+# ./fetcher.exe owl
+# ./fetcher.exe tapir
+# ./fetcher.exe elephant
+# ./fetcher.exe swimming
+# ./fetcher.exe skiing
+# ./fetcher.exe stonehenge
+# ./fetcher.exe pyramids
+
+
+# ./fetcher.exe guitar
+# ./fetcher.exe drums
+# ./fetcher.exe eye
+# ./fetcher.exe woods
+# ./fetcher.exe mountain
+# ./fetcher.exe river
+# ./fetcher.exe moon
+# ./fetcher.exe dolphin
+# ./fetcher.exe tree
+# ./fetcher.exe lightning
+# ./fetcher.exe clouds
+
+# ./fetcher.exe feather
+# ./fetcher.exe library
+# ./fetcher.exe apple
+# ./fetcher.exe casino
+# ./fetcher.exe desert
+# ./fetcher.exe cake
+# ./fetcher.exe mosaic
+# ./fetcher.exe skyscraper
+# ./fetcher.exe bridge
+# ./fetcher.exe boat
+# ./fetcher.exe train
+
+# after I accidentally deleted a bunch!
+# ./fetcher.exe tapir
+# ./fetcher.exe swimming
+# ./fetcher.exe running &
+# ./fetcher.exe sushi &
+# ./fetcher.exe woods &
+# ./fetcher.exe river &
+# ./fetcher.exe surfing &
+# ./fetcher.exe waterfall &
+# ./fetcher.exe sunset &
+# ./fetcher.exe mosaic &
+# ./fetcher.exe tree
+# ./fetcher.exe owl &
+# ./fetcher.exe stonehenge &
+# ./fetcher.exe lightning &
+# ./fetcher.exe minivan &
+# ./fetcher.exe train &
+# ./fetcher.exe mushroom &
+# ./fetcher.exe skyscraper &
+# ./fetcher.exe skiing &
+# ./fetcher.exe mountain &
+# ./fetcher.exe "moai statue"
+
+# ./fetcher.exe pyramids
+# ./fetcher.exe iceberg
+
+./fetcher.exe capitol &
+./fetcher.exe "jack o lantern" &
+./fetcher.exe "tennis racket"
