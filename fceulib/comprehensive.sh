7b32eb584436b89284e523e580f7ff44c0e1e190
tom7 <tom7@cs.cmu.edu>
2015-05-09 21:33:50 +0000

    Better way to do comprehensive test. Prevents the working copy from looking modified just because we started modifying the log before checking.

--- fceulib/comprehensive.sh
+++ fceulib/comprehensive.sh
@@ -8,12 +8,11 @@ echo " ******************************************************** "
 echo ""
 echo ""
 
+REVISION=`svnversion`
+
 echo "" >> comprehensive-log.txt
-echo -n "Running comprehensive tests at SVN version " >> comprehensive-log.txt
-svnversion >> comprehensive-log.txt
-echo -n "Starting at " >> comprehensive-log.txt
-date >> comprehensive-log.txt
+echo "Running comprehensive tests at SVN version " ${REVISION} >> comprehensive-log.txt
+echo "Starting at" `date` >> comprehensive-log.txt
 time ./emulator_test.exe --comprehensive || exit -1
-echo -n " ... Succeeded at " >> comprehensive-log.txt
-date >> comprehensive-log.txt
+echo -n " ... " ${REVISION} " Succeeded at " `date` >> comprehensive-log.txt
 svn st
