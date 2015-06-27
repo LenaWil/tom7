331f4364b1c63273dd23b08b86acdaf7be0c5393
tom7 <tom7@cs.cmu.edu>
2015-06-27 18:54:44 +0000

    New sharded version of comprehensive test.

--- fceulib/comprehensive.sh
+++ fceulib/comprehensive.sh
@@ -29,6 +29,30 @@ fi
 echo "" >> comprehensive-log.txt
 echo "Running comprehensive tests at SVN version " ${REVISION} >> comprehensive-log.txt
 echo "Starting at" `date` >> comprehensive-log.txt
-time ./emulator_test.exe --comprehensive || exit -1
-echo -n " ... " ${REVISION} " Succeeded at " `date` >> comprehensive-log.txt
+
+
+./emulator_test.exe --comprehensive --index 0 --modulus 10 || 
+  echo "Revision ${REVISION} shard " 0 " FAILED" >> comprehensive-log.txt &
+./emulator_test.exe --comprehensive --index 1 --modulus 10 || 
+  echo "Revision ${REVISION} shard " 1 " FAILED" >> comprehensive-log.txt &
+./emulator_test.exe --comprehensive --index 2 --modulus 10 || 
+  echo "Revision ${REVISION} shard " 2 " FAILED" >> comprehensive-log.txt &
+./emulator_test.exe --comprehensive --index 3 --modulus 10 || 
+  echo "Revision ${REVISION} shard " 3 " FAILED" >> comprehensive-log.txt &
+./emulator_test.exe --comprehensive --index 4 --modulus 10 || 
+  echo "Revision ${REVISION} shard " 4 " FAILED" >> comprehensive-log.txt &
+./emulator_test.exe --comprehensive --index 5 --modulus 10 || 
+  echo "Revision ${REVISION} shard " 5 " FAILED" >> comprehensive-log.txt &
+./emulator_test.exe --comprehensive --index 6 --modulus 10 || 
+  echo "Revision ${REVISION} shard " 6 " FAILED" >> comprehensive-log.txt &
+./emulator_test.exe --comprehensive --index 7 --modulus 10 || 
+  echo "Revision ${REVISION} shard " 7 " FAILED" >> comprehensive-log.txt &
+./emulator_test.exe --comprehensive --index 8 --modulus 10 || 
+  echo "Revision ${REVISION} shard " 8 " FAILED" >> comprehensive-log.txt &
+./emulator_test.exe --comprehensive --index 9 --modulus 10 || 
+  echo "Revision ${REVISION} shard " 9 " FAILED" >> comprehensive-log.txt &
+
+wait
+
+echo -n " ... " ${REVISION} " Finished at " `date` >> comprehensive-log.txt
 svn st
