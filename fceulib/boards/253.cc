ebe053bb2f866c13239eb51eeaea4cc3d6866ebf
tom7 <tom7@cs.cmu.edu>
2015-08-29 21:09:29 +0000

    253, ghostbusters63in1, vrc2and4

--- fceulib/boards/253.cc
+++ fceulib/boards/253.cc
@@ -20,142 +20,149 @@
 
 #include "mapinc.h"
 
-static uint8 chrlo[8], chrhi[8], prg[2], mirr, vlock;
-static int32 IRQa, IRQCount, IRQLatch, IRQClock;
-static uint8 *WRAM = nullptr;
-static uint32 WRAMSIZE;
-static uint8 *CHRRAM = nullptr;
-static uint32 CHRRAMSIZE;
+static constexpr int CHRRAMSIZE = 2048;
+static constexpr int WRAMSIZE = 8192;
 
-static vector<SFORMAT> StateRegs = {{chrlo, 8, "CHRL"},     {chrhi, 8, "CHRH"},
-                              {prg, 2, "PRGR"},       {&mirr, 1, "MIRR"},
-                              {&vlock, 1, "VLCK"},    {&IRQa, 4, "IRQA"},
-                              {&IRQCount, 4, "IRQC"}, {&IRQLatch, 4, "IRQL"},
-                              {&IRQClock, 4, "IRQK"}};
+namespace {
+struct Mapper253 : public CartInterface {
+  uint8 chrlo[8] = {}, chrhi[8] = {}, prg[2] = {}, mirr = 0, vlock = 0;
+  int32 IRQa = 0, IRQCount = 0, IRQLatch = 0, IRQClock = 0;
+  uint8 *WRAM = nullptr;
+  uint8 *CHRRAM = nullptr;
 
-static void Sync() {
-  uint8 i;
-  fceulib__.cart->setprg8r(0x10, 0x6000, 0);
-  fceulib__.cart->setprg8(0x8000, prg[0]);
-  fceulib__.cart->setprg8(0xa000, prg[1]);
-  fceulib__.cart->setprg8(0xc000, ~1);
-  fceulib__.cart->setprg8(0xe000, ~0);
-  for (i = 0; i < 8; i++) {
-    uint32 chr = chrlo[i] | (chrhi[i] << 8);
-    if (chrlo[i] == 0xc8) {
-      vlock = 0;
-      continue;
-    } else if (chrlo[i] == 0x88) {
-      vlock = 1;
-      continue;
+  void Sync() {
+    uint8 i;
+    fc->cart->setprg8r(0x10, 0x6000, 0);
+    fc->cart->setprg8(0x8000, prg[0]);
+    fc->cart->setprg8(0xa000, prg[1]);
+    fc->cart->setprg8(0xc000, ~1);
+    fc->cart->setprg8(0xe000, ~0);
+    for (i = 0; i < 8; i++) {
+      uint32 chr = chrlo[i] | (chrhi[i] << 8);
+      if (chrlo[i] == 0xc8) {
+	vlock = 0;
+	continue;
+      } else if (chrlo[i] == 0x88) {
+	vlock = 1;
+	continue;
+      }
+      if (((chrlo[i] == 4) || (chrlo[i] == 5)) && !vlock)
+	fc->cart->setchr1r(0x10, i << 10, chr & 1);
+      else
+	fc->cart->setchr1(i << 10, chr);
     }
-    if (((chrlo[i] == 4) || (chrlo[i] == 5)) && !vlock)
-      fceulib__.cart->setchr1r(0x10, i << 10, chr & 1);
-    else
-      fceulib__.cart->setchr1(i << 10, chr);
+    switch (mirr) {
+      case 0: fc->cart->setmirror(MI_V); break;
+      case 1: fc->cart->setmirror(MI_H); break;
+      case 2: fc->cart->setmirror(MI_0); break;
+      case 3: fc->cart->setmirror(MI_1); break;
+    }
   }
-  switch (mirr) {
-    case 0: fceulib__.cart->setmirror(MI_V); break;
-    case 1: fceulib__.cart->setmirror(MI_H); break;
-    case 2: fceulib__.cart->setmirror(MI_0); break;
-    case 3: fceulib__.cart->setmirror(MI_1); break;
-  }
-}
 
-static DECLFW(M253Write) {
-  if ((A >= 0xB000) && (A <= 0xE00C)) {
-    uint8 ind = ((((A & 8) | (A >> 8)) >> 3) + 2) & 7;
-    uint8 sar = A & 4;
-    chrlo[ind] = (chrlo[ind] & (0xF0 >> sar)) | ((V & 0x0F) << sar);
-    if (A & 4) chrhi[ind] = V >> 4;
+  void M253Write(DECLFW_ARGS) {
+    if ((A >= 0xB000) && (A <= 0xE00C)) {
+      uint8 ind = ((((A & 8) | (A >> 8)) >> 3) + 2) & 7;
+      uint8 sar = A & 4;
+      chrlo[ind] = (chrlo[ind] & (0xF0 >> sar)) | ((V & 0x0F) << sar);
+      if (A & 4) chrhi[ind] = V >> 4;
+      Sync();
+    } else
+      switch (A) {
+	case 0x8010:
+	  prg[0] = V;
+	  Sync();
+	  break;
+	case 0xA010:
+	  prg[1] = V;
+	  Sync();
+	  break;
+	case 0x9400:
+	  mirr = V & 3;
+	  Sync();
+	  break;
+	case 0xF000:
+	  fc->X->IRQEnd(FCEU_IQEXT);
+	  IRQLatch &= 0xF0;
+	  IRQLatch |= V & 0xF;
+	  break;
+	case 0xF004:
+	  fc->X->IRQEnd(FCEU_IQEXT);
+	  IRQLatch &= 0x0F;
+	  IRQLatch |= V << 4;
+	  break;
+	case 0xF008:
+	  fc->X->IRQEnd(FCEU_IQEXT);
+	  IRQClock = 0;
+	  IRQCount = IRQLatch;
+	  IRQa = V & 2;
+	  break;
+      }
+  }
+
+  void Power() override {
     Sync();
-  } else
-    switch (A) {
-      case 0x8010:
-        prg[0] = V;
-        Sync();
-        break;
-      case 0xA010:
-        prg[1] = V;
-        Sync();
-        break;
-      case 0x9400:
-        mirr = V & 3;
-        Sync();
-        break;
-      case 0xF000:
-        fceulib__.X->IRQEnd(FCEU_IQEXT);
-        IRQLatch &= 0xF0;
-        IRQLatch |= V & 0xF;
-        break;
-      case 0xF004:
-        fceulib__.X->IRQEnd(FCEU_IQEXT);
-        IRQLatch &= 0x0F;
-        IRQLatch |= V << 4;
-        break;
-      case 0xF008:
-        fceulib__.X->IRQEnd(FCEU_IQEXT);
-        IRQClock = 0;
-        IRQCount = IRQLatch;
-        IRQa = V & 2;
-        break;
-    }
-}
+    fc->fceu->SetReadHandler(0x6000, 0x7FFF, Cart::CartBR);
+    fc->fceu->SetWriteHandler(0x6000, 0x7FFF, Cart::CartBW);
+    fc->fceu->SetReadHandler(0x8000, 0xFFFF, Cart::CartBR);
+    fc->fceu->SetWriteHandler(0x8000, 0xFFFF, [](DECLFW_ARGS) {
+      ((Mapper253*)fc->fceu->cartiface)->M253Write(DECLFW_FORWARD);
+    });
+  }
 
-static void M253Power(FC *fc) {
-  Sync();
-  fceulib__.fceu->SetReadHandler(0x6000, 0x7FFF, Cart::CartBR);
-  fceulib__.fceu->SetWriteHandler(0x6000, 0x7FFF, Cart::CartBW);
-  fceulib__.fceu->SetReadHandler(0x8000, 0xFFFF, Cart::CartBR);
-  fceulib__.fceu->SetWriteHandler(0x8000, 0xFFFF, M253Write);
-}
+  void Close() override {
+    free(WRAM);
+    free(CHRRAM);
+    WRAM = CHRRAM = nullptr;
+  }
 
-static void M253Close(FC *fc) {
-  free(WRAM);
-  free(CHRRAM);
-  WRAM = CHRRAM = nullptr;
-}
-
-static void M253IRQ(FC *fc, int a) {
-  static constexpr int LCYCS = 341;
-  if (IRQa) {
-    IRQClock += a * 3;
-    if (IRQClock >= LCYCS) {
-      while (IRQClock >= LCYCS) {
-        IRQClock -= LCYCS;
-        IRQCount++;
-        if (IRQCount & 0x100) {
-          fceulib__.X->IRQBegin(FCEU_IQEXT);
-          IRQCount = IRQLatch;
-        }
+  void M253IRQ(int a) {
+    static constexpr int LCYCS = 341;
+    if (IRQa) {
+      IRQClock += a * 3;
+      if (IRQClock >= LCYCS) {
+	while (IRQClock >= LCYCS) {
+	  IRQClock -= LCYCS;
+	  IRQCount++;
+	  if (IRQCount & 0x100) {
+	    fc->X->IRQBegin(FCEU_IQEXT);
+	    IRQCount = IRQLatch;
+	  }
+	}
       }
     }
   }
-}
 
-static void StateRestore(FC *fc, int version) {
-  Sync();
-}
-
-void Mapper253_Init(CartInfo *info) {
-  info->Power = M253Power;
-  info->Close = M253Close;
-  fceulib__.X->MapIRQHook = M253IRQ;
-  fceulib__.fceu->GameStateRestore = StateRestore;
-
-  CHRRAMSIZE = 2048;
-  CHRRAM = (uint8 *)FCEU_gmalloc(CHRRAMSIZE);
-  fceulib__.cart->SetupCartCHRMapping(0x10, CHRRAM, CHRRAMSIZE, 1);
-  fceulib__.state->AddExState(CHRRAM, CHRRAMSIZE, 0, "CRAM");
-
-  WRAMSIZE = 8192;
-  WRAM = (uint8 *)FCEU_gmalloc(WRAMSIZE);
-  fceulib__.cart->SetupCartPRGMapping(0x10, WRAM, WRAMSIZE, 1);
-  fceulib__.state->AddExState(WRAM, WRAMSIZE, 0, "WRAM");
-  if (info->battery) {
-    info->SaveGame[0] = WRAM;
-    info->SaveGameLen[0] = WRAMSIZE;
+  static void StateRestore(FC *fc, int version) {
+    ((Mapper253 *)fc->fceu->cartiface)->Sync();
   }
+  Mapper253(FC *fc, CartInfo *info) : CartInterface(fc) {
+    fc->X->MapIRQHook = [](FC *fc, int a) {
+      ((Mapper253 *)fc->fceu->cartiface)->M253IRQ(a);
+    };
+    fc->fceu->GameStateRestore = StateRestore;
 
-  fceulib__.state->AddExVec(StateRegs);
+    CHRRAM = (uint8 *)FCEU_gmalloc(CHRRAMSIZE);
+    fc->cart->SetupCartCHRMapping(0x10, CHRRAM, CHRRAMSIZE, 1);
+    fc->state->AddExState(CHRRAM, CHRRAMSIZE, 0, "CRAM");
+
+    WRAM = (uint8 *)FCEU_gmalloc(WRAMSIZE);
+    fc->cart->SetupCartPRGMapping(0x10, WRAM, WRAMSIZE, 1);
+    fc->state->AddExState(WRAM, WRAMSIZE, 0, "WRAM");
+    if (info->battery) {
+      info->SaveGame[0] = WRAM;
+      info->SaveGameLen[0] = WRAMSIZE;
+    }
+
+    fc->state->AddExVec({{chrlo, 8, "CHRL"}, {chrhi, 8, "CHRH"},
+			 {prg, 2, "PRGR"}, {&mirr, 1, "MIRR"},
+			 {&vlock, 1, "VLCK"}, {&IRQa, 4, "IRQA"},
+			 {&IRQCount, 4, "IRQC"}, {&IRQLatch, 4, "IRQL"},
+			 {&IRQClock, 4, "IRQK"}});
+    
+  }
+};
+}
+
+CartInterface *Mapper253_Init(FC *fc, CartInfo *info) {
+  return new Mapper253(fc, info);
 }
--- fceulib/boards/vrc2and4.cc
+++ fceulib/boards/vrc2and4.cc
@@ -20,276 +20,295 @@
 
 #include "mapinc.h"
 
-static uint8 isPirate, is22;
-static uint16 IRQCount;
-static uint8 IRQLatch, IRQa;
-static uint8 prgreg[2], chrreg[8];
-static uint16 chrhi[8];
-static uint8 regcmd, irqcmd, mirr, big_bank;
-static uint16 acount = 0;
-static uint16 weirdo = 0;
+static constexpr int WRAMSIZE = 8192;
 
-static uint8 *WRAM = nullptr;
-static uint32 WRAMSIZE;
+namespace {
+struct VRC : public CartInterface {
 
-static vector<SFORMAT> StateRegs = {{prgreg, 2, "PREG"},
-                              {chrreg, 8, "CREG"},
-                              {chrhi, 16, "CRGH"},
-                              {&regcmd, 1, "CMDR"},
-                              {&irqcmd, 1, "CMDI"},
-                              {&mirr, 1, "MIRR"},
-                              {&big_bank, 1, "BIGB"},
-                              {&IRQCount, 2, "IRQC"},
-                              {&IRQLatch, 1, "IRQL"},
-                              {&IRQa, 1, "IRQA"}};
+  const bool isPirate;
+  const bool is22;
+  uint16 IRQCount = 0;
+  uint8 IRQLatch = 0, IRQa = 0;
+  uint8 prgreg[2] = {}, chrreg[8] = {};
+  uint16 chrhi[8] = {};
+  uint8 regcmd = 0, irqcmd = 0, mirr = 0, big_bank = 0;
+  uint16 acount = 0;
+  uint16 weirdo = 0;
 
-static void Sync() {
-  if (regcmd & 2) {
-    fceulib__.cart->setprg8(0xC000, prgreg[0] | big_bank);
-    fceulib__.cart->setprg8(0x8000, ((~1) & 0x1F) | big_bank);
-  } else {
-    fceulib__.cart->setprg8(0x8000, prgreg[0] | big_bank);
-    fceulib__.cart->setprg8(0xC000, ((~1) & 0x1F) | big_bank);
-  }
-  fceulib__.cart->setprg8(0xA000, prgreg[1] | big_bank);
-  fceulib__.cart->setprg8(0xE000, ((~0) & 0x1F) | big_bank);
-  if (fceulib__.unif->UNIFchrrama)
-    fceulib__.cart->setchr8(0);
-  else {
-    uint8 i;
-    if (!weirdo)
-      for (i = 0; i < 8; i++)
-        fceulib__.cart->setchr1(i << 10, (chrhi[i] | chrreg[i]) >> is22);
-    else {
-      fceulib__.cart->setchr1(0x0000, 0xFC);
-      fceulib__.cart->setchr1(0x0400, 0xFD);
-      fceulib__.cart->setchr1(0x0800, 0xFF);
-      weirdo--;
-    }
-  }
-  switch (mirr & 0x3) {
-    case 0: fceulib__.cart->setmirror(MI_V); break;
-    case 1: fceulib__.cart->setmirror(MI_H); break;
-    case 2: fceulib__.cart->setmirror(MI_0); break;
-    case 3: fceulib__.cart->setmirror(MI_1); break;
-  }
-}
+  uint8 *WRAM = nullptr;
 
-static DECLFW(VRC24Write) {
-  A &= 0xF003;
-  if ((A >= 0xB000) && (A <= 0xE003)) {
-    if (fceulib__.unif->UNIFchrrama) {
-      // my personally many-in-one feature ;) just for support pirate cart
-      // 2-in-1
-      big_bank = (V & 8) << 2;
+  void Sync() {
+    if (regcmd & 2) {
+      fc->cart->setprg8(0xC000, prgreg[0] | big_bank);
+      fc->cart->setprg8(0x8000, ((~1) & 0x1F) | big_bank);
     } else {
-      uint16 i = ((A >> 1) & 1) | ((A - 0xB000) >> 11);
-      uint16 nibble = ((A & 1) << 2);
-      chrreg[i] &= (0xF0) >> nibble;
-      chrreg[i] |= (V & 0xF) << nibble;
-      // another one many in one feature from pirate carts
-      if (nibble) chrhi[i] = (V & 0x10) << 4;
+      fc->cart->setprg8(0x8000, prgreg[0] | big_bank);
+      fc->cart->setprg8(0xC000, ((~1) & 0x1F) | big_bank);
     }
-    Sync();
-  } else {
-    switch (A & 0xF003) {
-      case 0x8000:
-      case 0x8001:
-      case 0x8002:
-      case 0x8003:
-        if (!isPirate) {
-          prgreg[0] = V & 0x1F;
-          Sync();
-        }
-        break;
-      case 0xA000:
-      case 0xA001:
-      case 0xA002:
-      case 0xA003:
-        if (!isPirate)
-          prgreg[1] = V & 0x1F;
-        else {
-          prgreg[0] = (V & 0x1F) << 1;
-          prgreg[1] = ((V & 0x1F) << 1) | 1;
-        }
-        Sync();
-        break;
-      case 0x9000:
-      case 0x9001:
-        if (V != 0xFF) mirr = V;
-        Sync();
-        break;
-      case 0x9002:
-      case 0x9003:
-        regcmd = V;
-        Sync();
-        break;
-      case 0xF000:
-        fceulib__.X->IRQEnd(FCEU_IQEXT);
-        IRQLatch &= 0xF0;
-        IRQLatch |= V & 0xF;
-        break;
-      case 0xF001:
-        fceulib__.X->IRQEnd(FCEU_IQEXT);
-        IRQLatch &= 0x0F;
-        IRQLatch |= V << 4;
-        break;
-      case 0xF002:
-        fceulib__.X->IRQEnd(FCEU_IQEXT);
-        acount = 0;
-        IRQCount = IRQLatch;
-        IRQa = V & 2;
-        irqcmd = V & 1;
-        break;
-      case 0xF003:
-        fceulib__.X->IRQEnd(FCEU_IQEXT);
-        IRQa = irqcmd;
-        break;
+    fc->cart->setprg8(0xA000, prgreg[1] | big_bank);
+    fc->cart->setprg8(0xE000, ((~0) & 0x1F) | big_bank);
+    if (fc->unif->UNIFchrrama)
+      fc->cart->setchr8(0);
+    else {
+      uint8 i;
+      if (!weirdo)
+	for (i = 0; i < 8; i++)
+	  fc->cart->setchr1(i << 10, (chrhi[i] | chrreg[i]) >> is22);
+      else {
+	fc->cart->setchr1(0x0000, 0xFC);
+	fc->cart->setchr1(0x0400, 0xFD);
+	fc->cart->setchr1(0x0800, 0xFF);
+	weirdo--;
+      }
+    }
+    switch (mirr & 0x3) {
+      case 0: fc->cart->setmirror(MI_V); break;
+      case 1: fc->cart->setmirror(MI_H); break;
+      case 2: fc->cart->setmirror(MI_0); break;
+      case 3: fc->cart->setmirror(MI_1); break;
     }
   }
-}
 
-static DECLFW(M21Write) {
-  A = (A & 0xF000) | ((A >> 1) & 0x3);
-  // Ganbare Goemon Gaiden 2 - Tenka no Zaihou (J) [!] isn't mapper 21
-  // actually, it's mapper 23 by wirings
-  VRC24Write(fc, A, V);
-}
-
-static DECLFW(M22Write) {
-  if (A == 0xC007) {
-    // Ganbare Goemon Gaiden does strange things!!! at the end credits
-    // quick dirty hack, seems there is no other games with such PCB, so
-    // we never know if it will not work for something else lol
-    weirdo = 8;
-  }
-  A |= ((A >> 2) & 0x3);
-  // It's just swapped lines from 21 mapper
-  VRC24Write(fc, (A & 0xF000) | ((A >> 1) & 1) | ((A << 1) & 2), V);
-}
-
-static DECLFW(M23Write) {
-  A |= ((A >> 2) & 0x3) | ((A >> 4) & 0x3) | ((A >> 6) & 0x3);
-  // actually there is many-in-one mapper source, some pirate or
-  // licensed games use various address bits for registers
-  VRC24Write(fc, A, V);
-}
-
-static void M21Power(FC *fc) {
-  Sync();
-  fceulib__.fceu->SetReadHandler(0x8000, 0xFFFF, Cart::CartBR);
-  fceulib__.fceu->SetWriteHandler(0x8000, 0xFFFF, M21Write);
-}
-
-static void M22Power(FC *fc) {
-  Sync();
-  fceulib__.fceu->SetReadHandler(0x8000, 0xFFFF, Cart::CartBR);
-  fceulib__.fceu->SetWriteHandler(0x8000, 0xFFFF, M22Write);
-}
-
-static void M23Power(FC *fc) {
-  big_bank = 0x20;
-  Sync();
-  fceulib__.cart->setprg8r(0x10, 0x6000, 0);  // Only two Goemon games are have
-                                              // battery backed RAM, three more
-                                              // shooters
-  // (Parodius Da!, Gradius 2 and Crisis Force uses 2k or SRAM at 6000-67FF only
-  fceulib__.fceu->SetReadHandler(0x6000, 0x7FFF, Cart::CartBR);
-  fceulib__.fceu->SetWriteHandler(0x6000, 0x7FFF, Cart::CartBW);
-  fceulib__.fceu->SetReadHandler(0x8000, 0xFFFF, Cart::CartBR);
-  fceulib__.fceu->SetWriteHandler(0x8000, 0xFFFF, M23Write);
-}
-
-static void M25Power(FC *fc) {
-  big_bank = 0x20;
-  Sync();
-  fceulib__.cart->setprg8r(0x10, 0x6000, 0);
-  fceulib__.fceu->SetReadHandler(0x6000, 0x7FFF, Cart::CartBR);
-  fceulib__.fceu->SetWriteHandler(0x6000, 0x7FFF, Cart::CartBW);
-  fceulib__.fceu->SetReadHandler(0x8000, 0xFFFF, Cart::CartBR);
-  fceulib__.fceu->SetWriteHandler(0x8000, 0xFFFF, M22Write);
-}
-
-void VRC24IRQHook(FC *fc, int a) {
-  static constexpr int LCYCS = 341;
-  if (IRQa) {
-    acount += a * 3;
-    if (acount >= LCYCS) {
-      while (acount >= LCYCS) {
-        acount -= LCYCS;
-        IRQCount++;
-        if (IRQCount & 0x100) {
-          fceulib__.X->IRQBegin(FCEU_IQEXT);
-          IRQCount = IRQLatch;
-        }
+  void VRC24Write(DECLFW_ARGS) {
+    A &= 0xF003;
+    if ((A >= 0xB000) && (A <= 0xE003)) {
+      if (fc->unif->UNIFchrrama) {
+	// my personally many-in-one feature ;) just for support pirate cart
+	// 2-in-1
+	big_bank = (V & 8) << 2;
+      } else {
+	uint16 i = ((A >> 1) & 1) | ((A - 0xB000) >> 11);
+	uint16 nibble = ((A & 1) << 2);
+	chrreg[i] &= (0xF0) >> nibble;
+	chrreg[i] |= (V & 0xF) << nibble;
+	// another one many in one feature from pirate carts
+	if (nibble) chrhi[i] = (V & 0x10) << 4;
+      }
+      Sync();
+    } else {
+      switch (A & 0xF003) {
+	case 0x8000:
+	case 0x8001:
+	case 0x8002:
+	case 0x8003:
+	  if (!isPirate) {
+	    prgreg[0] = V & 0x1F;
+	    Sync();
+	  }
+	  break;
+	case 0xA000:
+	case 0xA001:
+	case 0xA002:
+	case 0xA003:
+	  if (!isPirate)
+	    prgreg[1] = V & 0x1F;
+	  else {
+	    prgreg[0] = (V & 0x1F) << 1;
+	    prgreg[1] = ((V & 0x1F) << 1) | 1;
+	  }
+	  Sync();
+	  break;
+	case 0x9000:
+	case 0x9001:
+	  if (V != 0xFF) mirr = V;
+	  Sync();
+	  break;
+	case 0x9002:
+	case 0x9003:
+	  regcmd = V;
+	  Sync();
+	  break;
+	case 0xF000:
+	  fc->X->IRQEnd(FCEU_IQEXT);
+	  IRQLatch &= 0xF0;
+	  IRQLatch |= V & 0xF;
+	  break;
+	case 0xF001:
+	  fc->X->IRQEnd(FCEU_IQEXT);
+	  IRQLatch &= 0x0F;
+	  IRQLatch |= V << 4;
+	  break;
+	case 0xF002:
+	  fc->X->IRQEnd(FCEU_IQEXT);
+	  acount = 0;
+	  IRQCount = IRQLatch;
+	  IRQa = V & 2;
+	  irqcmd = V & 1;
+	  break;
+	case 0xF003:
+	  fc->X->IRQEnd(FCEU_IQEXT);
+	  IRQa = irqcmd;
+	  break;
       }
     }
   }
-}
 
-static void StateRestore(FC *fc, int version) {
-  Sync();
-}
-
-static void VRC24Close(FC *fc) {
-  free(WRAM);
-  WRAM = nullptr;
-}
-
-void Mapper21_Init(CartInfo *info) {
-  isPirate = 0;
-  is22 = 0;
-  info->Power = M21Power;
-  fceulib__.X->MapIRQHook = VRC24IRQHook;
-  fceulib__.fceu->GameStateRestore = StateRestore;
-
-  fceulib__.state->AddExVec(StateRegs);
-}
-
-void Mapper22_Init(CartInfo *info) {
-  isPirate = 0;
-  is22 = 1;
-  info->Power = M22Power;
-  fceulib__.fceu->GameStateRestore = StateRestore;
-
-  fceulib__.state->AddExVec(StateRegs);
-}
-
-void VRC24_Init(CartInfo *info) {
-  info->Close = VRC24Close;
-  fceulib__.X->MapIRQHook = VRC24IRQHook;
-  fceulib__.fceu->GameStateRestore = StateRestore;
-
-  WRAMSIZE = 8192;
-  WRAM = (uint8 *)FCEU_gmalloc(WRAMSIZE);
-  fceulib__.cart->SetupCartPRGMapping(0x10, WRAM, WRAMSIZE, 1);
-  fceulib__.state->AddExState(WRAM, WRAMSIZE, 0, "WRAM");
-
-  if (info->battery) {
-    info->SaveGame[0] = WRAM;
-    info->SaveGameLen[0] = WRAMSIZE;
+  void M22Write(DECLFW_ARGS) {
+    if (A == 0xC007) {
+      // Ganbare Goemon Gaiden does strange things!!! at the end credits
+      // quick dirty hack, seems there is no other games with such PCB, so
+      // we never know if it will not work for something else lol
+      weirdo = 8;
+    }
+    A |= ((A >> 2) & 0x3);
+    // It's just swapped lines from 21 mapper
+    VRC24Write(fc, (A & 0xF000) | ((A >> 1) & 1) | ((A << 1) & 2), V);
+  }
+  
+  void VRC24IRQHook(int a) {
+    static constexpr int LCYCS = 341;
+    if (IRQa) {
+      acount += a * 3;
+      if (acount >= LCYCS) {
+	while (acount >= LCYCS) {
+	  acount -= LCYCS;
+	  IRQCount++;
+	  if (IRQCount & 0x100) {
+	    fc->X->IRQBegin(FCEU_IQEXT);
+	    IRQCount = IRQLatch;
+	  }
+	}
+      }
+    }
   }
 
-  fceulib__.state->AddExVec(StateRegs);
+  static void StateRestore(FC *fc, int version) {
+    ((VRC *)fc->fceu->cartiface)->Sync();
+  }
+
+  VRC(FC *fc, CartInfo *info, bool isp, bool is22) :
+    CartInterface(fc), isPirate(isp), is22(is22) {
+    fc->state->AddExVec({{prgreg, 2, "PREG"},
+			 {chrreg, 8, "CREG"},
+			 {chrhi, 16, "CRGH"},
+			 {&regcmd, 1, "CMDR"},
+			 {&irqcmd, 1, "CMDI"},
+			 {&mirr, 1, "MIRR"},
+			 {&big_bank, 1, "BIGB"},
+			 {&IRQCount, 2, "IRQC"},
+			 {&IRQLatch, 1, "IRQL"},
+			 {&IRQa, 1, "IRQA"}});
+    fc->fceu->GameStateRestore = StateRestore;
+  }
+};
+
+struct Mapper21 : public VRC {
+  void M21Write(DECLFW_ARGS) {
+    A = (A & 0xF000) | ((A >> 1) & 0x3);
+    // Ganbare Goemon Gaiden 2 - Tenka no Zaihou (J) [!] isn't mapper 21
+    // actually, it's mapper 23 by wirings
+    VRC24Write(fc, A, V);
+  }
+  
+  void Power() override {
+    Sync();
+    fc->fceu->SetReadHandler(0x8000, 0xFFFF, Cart::CartBR);
+    fc->fceu->SetWriteHandler(0x8000, 0xFFFF, [](DECLFW_ARGS) {
+      ((Mapper21*)fc->fceu->cartiface)->M21Write(DECLFW_FORWARD);
+    });
+  }
+
+  Mapper21(FC *fc, CartInfo *info) : VRC(fc, info, false, false) {
+    fc->X->MapIRQHook = [](FC *fc, int a) {
+      ((Mapper21 *)fc->fceu->cartiface)->VRC24IRQHook(a);
+    };
+  }
+};
+
+struct Mapper22 : public VRC {
+  void Power() override {
+    Sync();
+    fc->fceu->SetReadHandler(0x8000, 0xFFFF, Cart::CartBR);
+    fc->fceu->SetWriteHandler(0x8000, 0xFFFF, [](DECLFW_ARGS) {
+      ((Mapper22*)fc->fceu->cartiface)->M22Write(DECLFW_FORWARD);
+    });
+  }
+
+  Mapper22(FC *fc, CartInfo *info) : VRC(fc, info, false, true) {}
+};
+
+struct VRC24 : public VRC {
+  void Close() override {
+    free(WRAM);
+    WRAM = nullptr;
+  }
+
+  VRC24(FC *fc, CartInfo *info, bool isp, bool is22) :
+    VRC(fc, info, isp, is22) {
+    fc->X->MapIRQHook = [](FC *fc, int a) {
+      ((VRC24 *)fc->fceu->cartiface)->VRC24IRQHook(a);
+    };
+
+    WRAM = (uint8 *)FCEU_gmalloc(WRAMSIZE);
+    fc->cart->SetupCartPRGMapping(0x10, WRAM, WRAMSIZE, 1);
+    fc->state->AddExState(WRAM, WRAMSIZE, 0, "WRAM");
+
+    if (info->battery) {
+      info->SaveGame[0] = WRAM;
+      info->SaveGameLen[0] = WRAMSIZE;
+    }
+  }
+};
+
+struct Mapper23 : public VRC24 {
+  using VRC24::VRC24;
+  void M23Write(DECLFW_ARGS) {
+    A |= ((A >> 2) & 0x3) | ((A >> 4) & 0x3) | ((A >> 6) & 0x3);
+    // actually there is many-in-one mapper source, some pirate or
+    // licensed games use various address bits for registers
+    VRC24Write(fc, A, V);
+  }
+
+  void Power() override {
+    big_bank = 0x20;
+    Sync();
+    // Only two Goemon games are have
+    // battery backed RAM, three more
+    // shooters
+    fc->cart->setprg8r(0x10, 0x6000, 0);  
+    // (Parodius Da!, Gradius 2 and Crisis Force uses 2k or SRAM at
+    // 6000-67FF only
+    fc->fceu->SetReadHandler(0x6000, 0x7FFF, Cart::CartBR);
+    fc->fceu->SetWriteHandler(0x6000, 0x7FFF, Cart::CartBW);
+    fc->fceu->SetReadHandler(0x8000, 0xFFFF, Cart::CartBR);
+    fc->fceu->SetWriteHandler(0x8000, 0xFFFF, [](DECLFW_ARGS) {
+      ((Mapper23*)fc->fceu->cartiface)->M23Write(DECLFW_FORWARD);
+    });
+  }
+};
+
+struct Mapper25 : public VRC24 {
+  using VRC24::VRC24;
+  void Power() override {
+    big_bank = 0x20;
+    Sync();
+    fc->cart->setprg8r(0x10, 0x6000, 0);
+    fc->fceu->SetReadHandler(0x6000, 0x7FFF, Cart::CartBR);
+    fc->fceu->SetWriteHandler(0x6000, 0x7FFF, Cart::CartBW);
+    fc->fceu->SetReadHandler(0x8000, 0xFFFF, Cart::CartBR);
+    fc->fceu->SetWriteHandler(0x8000, 0xFFFF, [](DECLFW_ARGS) {
+      ((Mapper25*)fc->fceu->cartiface)->M22Write(DECLFW_FORWARD);
+    });
+  }
+};
 }
 
-void Mapper23_Init(CartInfo *info) {
-  isPirate = 0;
-  is22 = 0;
-  info->Power = M23Power;
-  VRC24_Init(info);
+CartInterface *Mapper21_Init(FC *fc, CartInfo *info) {
+  return new Mapper21(fc, info);
 }
 
-void Mapper25_Init(CartInfo *info) {
-  isPirate = 0;
-  is22 = 0;
-  info->Power = M25Power;
-  VRC24_Init(info);
+CartInterface *Mapper22_Init(FC *fc, CartInfo *info) {
+  return new Mapper22(fc, info);
 }
 
-void UNLT230_Init(CartInfo *info) {
-  isPirate = 1;
-  is22 = 0;
-  info->Power = M23Power;
-  VRC24_Init(info);
+CartInterface *Mapper23_Init(FC *fc, CartInfo *info) {
+  return new Mapper23(fc, info, false, false);
+}
+
+CartInterface *Mapper25_Init(FC *fc, CartInfo *info) {
+  return new Mapper25(fc, info, false, false);
+}
+
+CartInterface *UNLT230_Init(FC *fc, CartInfo *info) {
+  return new Mapper23(fc, info, true, false);
 }
