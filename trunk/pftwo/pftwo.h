981e7b93c01dad5e9380f234f5dc8136ecf97d26
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2015-10-04 16:26:02 +0000

    Make compile with motifs/objectives

---
+++ trunk/pftwo/pftwo.h
@@ -0,0 +1,16 @@
+
+#ifndef __PFTWO_H
+#define __PFTWO_H
+
+#include <string>
+#include <vector>
+#include <map>
+#include <cstdint>
+
+using namespace std;
+
+#include "../fceulib/types.h"
+#include "../cc-lib/base/stringprintf.h"
+#include "../cc-lib/base/logging.h"
+
+#endif
