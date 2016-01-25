6ee56cca4a85a4c261fa33b5dbdf0e46890cf7f9
tom7 <tom7@be8137f2-001b-0410-b4be-9de0ac3fc846>
2016-01-25 03:45:20 +0000

    Fix bug in n-markov-controller; make warmup settable from config

--- trunk/pftwo/problem-twoplayer.cc
+++ trunk/pftwo/problem-twoplayer.cc
@@ -10,13 +10,9 @@
 
 // Note NMarkovController::Stats; making this large will often
 // make the matrix very sparse.
-static constexpr int MARKOV_N = 2;
+static constexpr int MARKOV_N = 3;
 
 static_assert(MARKOV_N >= 0 && MARKOV_N <= 8, "allowed range");
-
-// XXX for contra to enter konami code.
-// should be derived from input.
-static constexpr int WARMUP_FRAMES = 800;
 
 // ??!?
 static constexpr int OBSERVATION_SAMPLES = 32768;
@@ -34,11 +30,36 @@ TPP::Input Worker::RandomInput(ArcFour *rc) {
 }
 
 TPP::Input TPP::InputGenerator::RandomInput(ArcFour *rc) {
-  const uint8 p1 = tpp->markov1->RandomNext(prev1, rc);
-  const uint8 p2 = tpp->markov2->RandomNext(prev2, rc);
-  prev1 = tpp->markov1->Push(prev1, p1);
-  prev2 = tpp->markov2->Push(prev2, p2);
-  return MakeInput(p1, p2);
+  switch (type) {
+  default:
+  case Type::JUMP_LEFT:
+
+    if (counter < 60) {
+    auto Gen = [](int c) -> uint8 {
+      return
+	  // Hold left
+	  INPUT_L |
+	  // Release A occasionally to restart jump
+	  ((c & 31) == 0 ? 0 : INPUT_A) |
+	  // Press shoot rapidly
+	  ((c >> 1) & 1 ? 0 : INPUT_B) |
+	  // Alternate pressing up and not
+	  ((c >> 5) & 1 ? 0 : INPUT_U);
+    };
+    const uint8 p1 = Gen(counter);
+    const uint8 p2 = Gen(counter + 7);
+    counter++;
+    return MakeInput(p1, p2);
+  }
+  // FALLTHROUGH
+  case Type::MARKOV: {
+    const uint8 p1 = tpp->markov1->RandomNext(prev1, rc);
+    const uint8 p2 = tpp->markov2->RandomNext(prev2, rc);
+    prev1 = tpp->markov1->Push(prev1, p1);
+    prev2 = tpp->markov2->Push(prev2, p2);
+    return MakeInput(p1, p2);
+  }
+  }
 }
 
 void TPP::SaveSolution(const string &filename_part,
@@ -46,7 +67,7 @@ void TPP::SaveSolution(const string &filename_part,
 		       const State &state,
 		       const string &info) {
   vector<pair<uint8, uint8>> all_inputs;
-  for (int i = 0; i < WARMUP_FRAMES; i++) {
+  for (int i = 0; i < warmup_frames; i++) {
     all_inputs.push_back(original_inputs[i]);
   }
   for (const Input &input : inputs) {
@@ -65,19 +86,27 @@ TPP::TwoPlayerProblem(const map<string, string> &config) {
   game = GetDefault(config, "game", "");
   const string movie = GetDefault(config, "movie", "");
   original_inputs = SimpleFM2::ReadInputs2P(movie);
-  CHECK(original_inputs.size() > WARMUP_FRAMES);
+
+  warmup_frames = atoi(GetDefault(config, "warmup", "-1").c_str());
+  // XXX: Deduce this from input.
+  CHECK(warmup_frames >= 0) << "You must currently specify the "
+    "number of warmup frames as a config line 'warmup'. This is "
+    "the amount copied from the training data before automatic "
+    "play begins. 0 may work.";
+  
+  CHECK(original_inputs.size() > warmup_frames);
 
   // Throwaway emulator instance for warmup, training.
   printf("Warmup.\n");
   unique_ptr<Emulator> emu(Emulator::Create(game));
-  for (int i = 0; i < WARMUP_FRAMES; i++) {
+  for (int i = 0; i < warmup_frames; i++) {
     emu->Step(original_inputs[i].first, original_inputs[i].second);
   }
 
   printf("Get inputs.\n");
   // Now build inputs for training markov.
   vector<uint8> player1, player2;
-  for (int i = WARMUP_FRAMES; i < original_inputs.size(); i++) {
+  for (int i = warmup_frames; i < original_inputs.size(); i++) {
     const auto &p = original_inputs[i];
     player1.push_back(p.first);
     player2.push_back(p.second);
@@ -98,8 +127,8 @@ TPP::TwoPlayerProblem(const map<string, string> &config) {
   // Now execute inputs to get memories for learnfun.
   printf("Get memories.\n");
   vector<vector<uint8>> memories;
-  memories.reserve(original_inputs.size() - WARMUP_FRAMES);
-  for (int i = WARMUP_FRAMES; i < original_inputs.size(); i++) {
+  memories.reserve(original_inputs.size() - warmup_frames);
+  for (int i = warmup_frames; i < original_inputs.size(); i++) {
     const auto &p = original_inputs[i];
     emu->Step(p.first, p.second);
     memories.push_back(emu->GetMemory());
